64da440291ddaf557756b4960e18d26df07ed476
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-17 22:50:00 +0800

    background images should be different for each screen_  (A,B,C,D)

--- views/index.php
+++ views/index.php
@@ -9,212 +9,181 @@
 	<link href='https://fonts.googleapis.com/css?family=Ramabhadra' rel='stylesheet'>
 	<link rel="stylesheet" href="/css/foundation.min.css">
 	<link rel="stylesheet" href="/css/font-awesome.min.css">
-	<!-- <link rel="stylesheet" href="/css/slideshow.css"> -->
 	<link rel="stylesheet" href="/css/app.css">
 	<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
 	<script src="/js/foundation.min.js"></script>
 </head>
 <body>
-<ul class="cb-slideshow">
-	<li class="slideshow-1">
-		<span>Image 01</span>
-	</li>
-	<li class="slideshow-2">
-		<span>Image 02</span>
-	</li>
-	<li class="slideshow-3">
-		<span>Image 03</span>
-	</li>
-	<li class="slideshow-4">
-		<span>Image 04</span>
-	</li>
-</ul>
-<div data-sticky-container>
-	<div class="expanded row header" data-sticky data-options="marginTop:0;">
-		<div class="row nav-header">
-			<div class="columns nav-header-logo">
-				<div class="logo">
-					<a href="#"><img src="/images/ccc-logo-shadow.png" alt="Color Compass Corporation"></a>	
+	<ul class="cb-slideshow">
+		<li class="slideshow-1"><span>Image 01</span></li>
+		<li class="slideshow-2"><span>Image 02</span></li>
+		<li class="slideshow-3"><span>Image 03</span></li>
+		<li class="slideshow-4"><span>Image 04</span></li>
+	</ul>
+	<div data-sticky-container>
+		<div class="expanded row header" data-sticky data-options="marginTop:0;">
+			<div class="row nav-header">
+				<div class="columns nav-header-logo">
+					<div class="logo">
+						<a href="#"><img src="/images/ccc-logo-shadow.png" alt="Color Compass Corporation"></a>	
+					</div>
+				</div>
+				<div class="title-bar" data-responsive-toggle="top_menu" data-hide-for="large">
+		      <button class="menu-icon" type="button" data-toggle></button>
+		      <div class="title-bar-title">Menu</div>
+		    </div>
+				<div class="columns nav-header-menu">
+		      <div class="top-bar" id="top_menu">
+		        <div class="top-bar-left">
+							<ul class="dropdown vertical large-horizontal medium-vertical menu" data-responsive-menu="drilldown large-dropdown">
+								<li><a href="/">Home</a></li>
+								<li class="has-submenu">
+									<a href="">Company</a>
+									<ul class="submenu menu vertical" data-submenu>
+										<li><a href="/about">About Us</a></li>
+										<li><a href="/history">History</a></li>
+										<li><a href="/executive">Leadership Team</a></li>
+										<li><a href="/mission">Mission/Vision</a></li>
+									</ul>
+								</li>
+								<li class="has-submenu">
+									<a href="">Divisions</a>
+									<ul class="submenu menu vertical" data-submenu>
+										<li><a href="http://www.carlsonbodyshopsupply.com" target="_blank">Carlson Body Shop</a></li>
+										<li><a href="http://www.chaseautobodysupplies.com" target="_blank">Chase Auto Body</a></li>
+										<li><a href="http://www.pbedistributors.com" target="_blank">PBE Distributors</a></li>
+										<li><a href="http://www.rockymountainsales.ca" target="_blank">Rocky Mountain</a></li>
+										<li><a href="http://www.whiteandpeters.com" target="_blank">White &amp; Peters</a></li>
+									</ul>
+								</li>
+								<li><a href="/news">News</a></li>
+								<li><a href="/community">Community</a></li>
+								<li><a href="/contact">Contact</a></li>
+							</ul>
+		        </div>
+		      </div>
 				</div>
 			</div>
-			<div class="title-bar" data-responsive-toggle="top_menu" data-hide-for="large">
-	      <button class="menu-icon" type="button" data-toggle></button>
-	      <div class="title-bar-title">Menu</div>
+		</div>	
+	</div>
+
+	<div class="expanded row content">
+		<article id="screen_A">
+			<div>
+				<h3>Partners In The<br>Right Direction</h3>
+				<a href="#" class="home_next"></a>
+			</div>
+		</article>
+		<article id="screen_B">
+			<div>
+				<h3>An Automotive Refinish Supply company that specializes in the distribution of Auto Body Supplies, Collision Repair Training Services, and Specialty Market Coatings Nationwide</h3>
+				<a href="#" class="home_next"></a>
+			</div>
+		</article>
+		<article id="screen_C">
+			<div>
+				<h3>Our group delivers products, training, and solutions that enhance our customer’s production and profitability. We are a team of over 260 specialists throughout Canada who come together within four distinct Western Canadian jobber operations, as well as a national warehousing division; we have a national reach with a local touch.</h3>
+				<a href="#" class="home_next"></a>
+			</div>
+		</article>
+		<article id="screen_D">
+	    <div>
+	    	<ul>
+		      <li><a href="http://www.pbedistributors.com" target="_blank"><img src="/images/pbe_logo.png" alt="PBE Distributors"></a></li>           
+		      <li><a href="http://www.chaseautobodysupplies.com" target="_blank"><img src="/images/chase_logo.png" alt="Chase Auto Body"></a></li>            
+		      <li><a href="http://www.carlsonbodyshopsupply.com" target="_blank"><img src="/images/carlson_logo.png" alt="Carlson Body Shop"></a></li>            
+		      <li><a href="http://www.rockymountainsales.ca" target="_blank"><img src="/images/rocky_logo.png" alt="Rocky Mountain Sales"></a></li>
+		      <li><a href="http://www.whiteandpeters.com" target="_blank"><img src="/images/wp_logo.png" alt="White &amp; Peters"></a></li>
+		    </ul>
 	    </div>
-			<div class="columns nav-header-menu">
-	      <div class="top-bar" id="top_menu">
-	        <div class="top-bar-left">
-			<ul class="dropdown vertical large-horizontal medium-vertical menu" data-responsive-menu="drilldown large-dropdown">
-				<li><a href="/">Home</a></li>
-				<li class="has-submenu">
-					<a href="">Company</a>
-					<ul class="submenu menu vertical" data-submenu>
-						<li><a href="/about">About Us</a></li>
-						<li><a href="/history">History</a></li>
-						<li><a href="/executive">Leadership Team</a></li>
-						<li><a href="/mission">Mission/Vision</a></li>
-					</ul>
-				</li>
-				<li class="has-submenu">
-					<a href="">Divisions</a>
-					<ul class="submenu menu vertical" data-submenu>
-						<li><a href="http://www.carlsonbodyshopsupply.com" target="_blank">Carlson Body Shop</a></li>
-						<li><a href="http://www.chaseautobodysupplies.com" target="_blank">Chase Auto Body</a></li>
-						<li><a href="http://www.pbedistributors.com" target="_blank">PBE Distributors</a></li>
-						<li><a href="http://www.rockymountainsales.ca" target="_blank">Rocky Mountain</a></li>
-						<li><a href="http://www.whiteandpeters.com" target="_blank">White &amp; Peters</a></li>
-					</ul>
-				</li>
-				<li><a href="/news">News</a></li>
-				<li><a href="/community">Community</a></li>
-				<li><a href="/contact">Contact</a></li>
-			</ul>
-	        </div>
-	      </div>
-			</div>
-		</div>
-	</div>	
-</div>
+		</article>
+	</div>
 
-<div class="expanded row content">
-	<article id="screen_A">
-		<div>
-			<h3>Partners In The<br>Right Direction</h3>
-			<a href="#" class="home_next"></a>
-		</div>
-	</article>
-	<article id="screen_B">
-		<div>
-			<h3>An Automotive Refinish Supply company that specializes in the distribution of Auto Body Supplies, Collision Repair Training Services, and Specialty Market Coatings Nationwide</h3>
-			<a href="#" class="home_next"></a>
-		</div>
-	</article>
-	<article id="screen_C">
-		<div>
-			<h3>Our group delivers products, training, and solutions that enhance our customer’s production and profitability. We are a team of over 260 specialists throughout Canada who come together within four distinct Western Canadian jobber operations, as well as a national warehousing division; we have a national reach with a local touch.</h3>
-			<a href="#" class="home_next"></a>
-		</div>
-	</article>
-	<article id="screen_D">
-    <div>
-    	<ul>
-	      <li>
-	      	<a href="http://www.pbedistributors.com" target="_blank">
-	      		<img src="/images/pbe_logo.png" alt="PBE Distributors">
-	      	</a>
-	      </li>           
-	      <li>
-	      	<a href="http://www.chaseautobodysupplies.com" target="_blank">
-	      		<img src="/images/chase_logo.png" alt="Chase Auto Body">
-	      	</a>
-	      </li>            
-	      <li>
-	      	<a href="http://www.carlsonbodyshopsupply.com" target="_blank">
-	      		<img src="/images/carlson_logo.png" alt="Carlson Body Shop">
-	      	</a>
-	      </li>            
-	      <li>
-	        <a href="http://www.rockymountainsales.ca" target="_blank">
-	        	<img src="/images/rocky_logo.png" alt="Rocky Mountain Sales">
-	        </a>
-	      </li>
-	      <li>
-	        <a href="http://www.whiteandpeters.com" target="_blank">
-	        	<img src="/images/wp_logo.png" alt="White &amp; Peters">
-	        </a>
-	      </li>
-	    </ul>
-    </div>
-	</article>
-</div>
-
-<div class="expanded row footer">
-	<div class="row">
-		<div class="medium-12 large-4 columns">
-			<div class="corporation">
-				<h5>Color Compass Corporation</h5>
-				<p class="smaller">
-					&copy; <?php echo date('Y'); ?> by Color Compass Corporation
-				</p>
-				<p class="social">
-					<a href="https://www.linkedin.com/company/color-compass-corporation" target="_blank">
-						<i class="fa fa-linkedin-square fa-2x" aria-hidden="true" title="Color Compass Corporation LinkedIn"></i>
-					</a>
-					<a href="https://twitter.com/ColorCompass" target="_blank">
-						<i class="fa fa-twitter-square fa-2x" aria-hidden="true" title="Color Compass Corporation Twitter"></i>
-					</a>
-					<a href="https://www.facebook.com/Color-Compass-Corporation-660498810765347/" target="_blank">
-						<i class="fa fa-facebook-square fa-2x" aria-hidden="true" title="Color Compass Corporation FaceBook"></i>
-					</a>
-					<a href="https://plus.google.com/118393445120103564064/posts" target="_blank">
-						<i class="fa fa-google-plus-square fa-2x" aria-hidden="true" title="Color Compass Corporation Google +"></i>
-					</a>
-					<a href="https://www.instagram.com/color_compass/" target="_blank">
-						<i class="fa fa-instagram fa-2x" aria-hidden="true" title="Color Compass Corporation Instagram"></i>
-					</a>
-				</p>
+	<div class="expanded row footer">
+		<div class="row">
+			<div class="medium-12 large-4 columns">
+				<div class="corporation">
+					<h5>Color Compass Corporation</h5>
+					<p class="smaller">&copy; <?php echo date('Y'); ?> by Color Compass Corporation</p>
+					<p class="social">
+						<a href="https://www.linkedin.com/company/color-compass-corporation" target="_blank">
+							<i class="fa fa-linkedin-square fa-2x" aria-hidden="true" title="Color Compass Corporation LinkedIn"></i>
+						</a>
+						<a href="https://twitter.com/ColorCompass" target="_blank">
+							<i class="fa fa-twitter-square fa-2x" aria-hidden="true" title="Color Compass Corporation Twitter"></i>
+						</a>
+						<a href="https://www.facebook.com/Color-Compass-Corporation-660498810765347/" target="_blank">
+							<i class="fa fa-facebook-square fa-2x" aria-hidden="true" title="Color Compass Corporation FaceBook"></i>
+						</a>
+						<a href="https://plus.google.com/118393445120103564064/posts" target="_blank">
+							<i class="fa fa-google-plus-square fa-2x" aria-hidden="true" title="Color Compass Corporation Google +"></i>
+						</a>
+						<a href="https://www.instagram.com/color_compass/" target="_blank">
+							<i class="fa fa-instagram fa-2x" aria-hidden="true" title="Color Compass Corporation Instagram"></i>
+						</a>
+					</p>
+				</div>
 			</div>
-		</div>
-		<div class="medium-6 large-3 large-offset-2 columns">
-			<div class="office-1">
-				<h5>Vancouver Office</h5>
-				<p>
-					1368 United Blvd.<br>
-					Coquitlam BC, Canada<br>
-					V3K 6Y2<br>
-					604.540.7200
-				</p>
+			<div class="medium-6 large-3 large-offset-2 columns">
+				<div class="office-1">
+					<h5>Vancouver Office</h5>
+					<p>1368 United Blvd.<br>Coquitlam BC, Canada<br>V3K 6Y2<br>604.540.7200</p>
+				</div>
 			</div>
-		</div>
-		<div class="medium-6 large-3 columns">
-			<div class="office-2">
-				<h5>Edmonton Office</h5>
-				<p>
-					5308 - 97 Street<br>
-					Edmonton AB, Canada<br>
-					T6E 5W5<br>
-					780.438.0838
-				</p>
+			<div class="medium-6 large-3 columns">
+				<div class="office-2">
+					<h5>Edmonton Office</h5>
+					<p>5308 - 97 Street<br>Edmonton AB, Canada<br>T6E 5W5<br>780.438.0838</p>
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
 
 	<script>
 		$(document).foundation();
+
 		// $('.header').on('sticky.zf.stuckto:top', function(){
 		//   $(this).addClass('shrink');
 		// }).on('sticky.zf.unstuckfrom:top', function(){
 		//   $(this).removeClass('shrink');
 		// });
+
 		$(document).ready(function() {
+			//article transition when arrow button click
 			$('.content .home_next').on('click', function(e) {
 				e.preventDefault();
 				e.stopPropagation();
+
+				var windowWidth = $(window).width();
 				var height = $('.content article').height();
 				var index = $('.content article').index($(this).parent('div').parent('article')) + 1;
 				var headerHeight = $('.header').height();
-				// console.log(headerHeight)
-				var scrollTop = height * index - headerHeight;
+				var scrollTop = 0;
+				if (windowWidth > 640) {
+					scrollTop = height * index - headerHeight;
+				} else {
+					scrollTop = height * index;
+				}
 				$("html, body").animate({
 		      scrollTop: scrollTop
-		    }, 1500);
-
-				$('.cb-slideshow li span').css('opacity', 0);
-				$('.cb-slideshow li:nth-child('+(index)+') span').css('animation', 'imageAnimation1 1s linear 0s');
-				$('.cb-slideshow li:nth-child('+(index+1)+') span').css('animation', 'imageAnimation2 1s linear 0s');
-				$('.cb-slideshow li:nth-child('+(index+1)+') span').css('opacity', 1);
+		    }, 1000);
 			});
-			$("article").on('mouseenter', function(e) {
+
+			//background images transition for each screen_(A,B,C,D) 
+			var prevArticleIndex = 1;
+			$(".content article").on('mouseenter', function(e) {
 				e.preventDefault();
 				e.stopPropagation();
-	      console.log($(this).attr('id'))
-	      var index = $('.content article').index($(this));
-	      console.log(index)
-	      $('.cb-slideshow li span').css('opacity', 0);
-				$('.cb-slideshow li:nth-child('+(index)+') span').css('animation', 'imageAnimation1 1s linear 0s');
-				$('.cb-slideshow li:nth-child('+(index+1)+') span').css('animation', 'imageAnimation2 1s linear 0s');
-				$('.cb-slideshow li:nth-child('+(index+1)+') span').css('opacity', 1);
+	      var index = $('.content article').index($(this))+1;
+
+	      if (prevArticleIndex == index) return;
+
+				$('.cb-slideshow li:nth-child('+prevArticleIndex+') span').css('animation', 'imageAnimation1 1s linear 0s');
+				$('.cb-slideshow li:nth-child('+prevArticleIndex+') span').css('opacity', 0);
+				$('.cb-slideshow li:nth-child('+index+') span').css('animation', 'imageAnimation2 1s linear 0s');
+				$('.cb-slideshow li:nth-child('+index+') span').css('opacity', 1);
+
+				prevArticleIndex = index;
 	    });
 		});
 	</script>
