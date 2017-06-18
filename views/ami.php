d14ef5fb7fc737f19c5685fd07d43e3e060bdc20
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-18 22:38:36 +0800

    add ami.php

---
+++ views/ami.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"/>
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>AMI | AUTOMOTIVE MANAGEMENT INSTITUTE</title>
+	<link rel="stylesheet" href="/css/foundation.min.css">
+	<link rel="stylesheet" href="/css/font-awesome.min.css">
+	<link rel="stylesheet" href="/css/app.css">
+	<script src="//code.jquery.com/jquery-2.1.4.min.js"></script>
+	<script src="/js/foundation.min.js"></script>
+</head>
+<body>
+	<div data-sticky-container>
+		<div class="expanded row header" data-sticky data-options="marginTop:0;">
+			<div class="row nav-header">
+				<div class="columns">
+					<div class="logo">
+						<a href="#"><img src="/images/ccc-logo-shadow.png" alt="Color Compass Corporation"></a>	
+					</div>
+				</div>
+				<div class="title-bar" data-responsive-toggle="top_menu" data-hide-for="large">
+		      <button class="menu-icon" type="button" data-toggle></button>
+		      <div class="title-bar-title">Menu</div>
+		    </div>
+				<div class="columns nav-header-menu">
+		      <div class="top-bar" id="top_menu">
+		        <div class="top-bar-left">
+							<ul class="dropdown vertical large-horizontal medium-vertical menu" data-responsive-menu="drilldown large-dropdown">
+								<li><a href="/">Home</a></li>
+								<li class="has-submenu">
+									<a href="">Company</a>
+									<ul class="submenu menu vertical" data-submenu>
+										<li><a href="/about">About Us</a></li>
+										<li><a href="/history">History</a></li>
+										<li><a href="/executive">Leadership Team</a></li>
+										<li><a href="/mission">Mission/Vision</a></li>
+									</ul>
+								</li>
+								<li class="has-submenu">
+									<a href="">Divisions</a>
+									<ul class="submenu menu vertical" data-submenu>
+										<li><a href="http://www.carlsonbodyshopsupply.com" target="_blank">Carlson Body Shop</a></li>
+										<li><a href="http://www.chaseautobodysupplies.com" target="_blank">Chase Auto Body</a></li>
+										<li><a href="http://www.pbedistributors.com" target="_blank">PBE Distributors</a></li>
+										<li><a href="http://www.rockymountainsales.ca" target="_blank">Rocky Mountain</a></li>
+										<li><a href="http://www.whiteandpeters.com" target="_blank">White &amp; Peters</a></li>
+									</ul>
+								</li>
+								<li><a href="/news">News</a></li>
+								<li><a href="/community">Community</a></li>
+								<li><a href="/contact">Contact</a></li>
+							</ul>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>	
+	</div>
+</body>
+<script type="text/javascript">
+	
+</script>
+</html>
