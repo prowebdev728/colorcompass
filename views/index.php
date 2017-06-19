4b1f49b0ab33cd72d143116ca5b940d63c5a7a26
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-19 20:09:43 +0800

    adjustment

--- views/index.php
+++ views/index.php
@@ -165,21 +165,17 @@
 		      scrollTop: scrollTop
 		    }, 1000);
 			});
-
 			//background images transition for each screen_(A,B,C,D) 
 			/*var prevArticleIndex = 1;
 			$(".content article").on('mouseenter', function(e) {
 				e.preventDefault();
 				e.stopPropagation();
 	      var index = $('.content article').index($(this))+1;
-
 	      if (prevArticleIndex == index) return;
-
 				$('.cb-slideshow li:nth-child('+prevArticleIndex+') span').css('animation', 'imageAnimation1 1s linear 0s');
 				$('.cb-slideshow li:nth-child('+prevArticleIndex+') span').css('opacity', 0);
 				$('.cb-slideshow li:nth-child('+index+') span').css('animation', 'imageAnimation2 1s linear 0s');
 				$('.cb-slideshow li:nth-child('+index+') span').css('opacity', 1);
-
 				prevArticleIndex = index;
 	    });*/
 		});
