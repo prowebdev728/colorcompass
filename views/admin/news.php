7852a9c3c7fd3ecae77db9c842e9c364bcf60938
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-23 12:42:36 +0800

    add

---
+++ views/admin/news.php
@@ -0,0 +1,27 @@
+<?php include 'views/admin/header.php'; ?>
+<h2>News Articles List <span class="padded-left"><a href="<?=$appurl.'/home/edit/0'?>" class="button small">Add Article</a></span></h2>
+<div class="datalist">
+<table id="datalist" class="display">
+<thead>
+	<tr>
+		<th>URL</th>
+		<th>Meta Title</th>
+		<th>Date</th>
+		<th>Status</th>
+		<th>Action</th>
+	</tr>
+	</thead>
+	<tbody>
+	<?php foreach($data as $row): ?>
+	<tr>
+		<td><?=$row['url'];?></td>
+		<td><?=$row['title'];?></td>
+		<td><?=$row['date'];?></td>
+		<td><?=$row['status'];?></td>
+		<td><?=$row['action'];?></td>
+	</tr>
+	<?php endforeach; ?>
+	</tbody>
+</table>
+</div>
+<?php include 'views/admin/footer.php'; ?>
