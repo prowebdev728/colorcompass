c0c260bf966c4fc2196da006747289962cbcb9c0
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-26 08:30:20 +0800

    add

---
+++ views/emailuser/optnoemail.php
@@ -0,0 +1,6 @@
+<div class="m-w1">
+	<div class="m-w2 section-inner">
+		<h2 class="clear">Email Not Found</h2>
+		<p>We could not find your email in our database. Please <a href="/contact-us">contacts us</a> if you would like to be add to our list.</p>
+	</div>
+</div>
