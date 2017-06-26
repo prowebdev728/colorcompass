0fefeb3cdd2d3d48c82f4e99163310f06f57a7fd
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-26 08:30:34 +0800

    add

---
+++ views/error404.php
@@ -0,0 +1,79 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Color Compass Corporation</title>
+		<meta charset="utf-8">
+		<style>
+			*{
+				maring: 0;
+				padding: 0;
+			}
+			body{
+				font-family: 'Source Sans Pro', sans-serif;
+				background: #FFFFFF;
+				color: #000000;
+			}
+			a {
+				color: #000000;
+			}
+			a:hover{
+				color: #ACACAC;
+			}
+
+			#content{
+				margin: 0 auto;
+				width: 960px;
+			}
+
+			.clearfix:after {
+				content: ".";
+				display: block;
+				clear: both;
+				visibility: hidden;
+				line-height: 0;
+				height: 0;
+			}
+
+			.clearfix {
+				display: block;
+			}
+
+			#logo {
+				margin: 1em;
+				text-align: center;
+			}
+			#main_body{
+				text-align: center;
+			}
+			.enormous-font{
+				font-size: 5rem;
+				margin-bottom: 0em;
+			}
+			.big-font{
+				font-size: 2rem;
+			}
+			hr{
+				width: 25%;
+				height: 1px;
+				background: #1f3759;
+				border: 0px;
+			}
+		</style>
+	</head>
+	<body>
+		<div id="content">
+			<header>
+				<div id="logo">
+					<a href="/"><img src="images/logo1.png" width="319" height="71"></a>
+				</div>
+			</header>
+			<div class="clearfix"></div>
+			<div id="main_body">
+				<p class="enormous-font"> 404 </p>
+				<p class="big-font"> Sorry, but this page does not exist... </p>
+				<hr>
+				<p class="big-font"> Let’s return to the <a href="/">home page</a></p>
+			</div>
+		</div>
+	</body>
+</html>
