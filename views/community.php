335085777d5235141c8786781dabd21fd6eab03f
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-26 08:30:03 +0800

    add

---
+++ views/community.php
@@ -0,0 +1,37 @@
+<div class="row intro bpadding">
+	<img src="/images/community-banner.jpg" alt="Color Compass Corporation Community">
+</div>
+<div class="row">
+	<div class="small-12 column">
+		<h3>Community</h3>
+		<p>Color Compass continues to nurture the key community initiatives that it has held near and dear over the years. In addition to supporting the spontaneous requests of local charities in various efforts, The Color Compass Corporation has decided to focus its main community-building initiatives in two key areas: health and education. Through supporting local and national healthcare facilities and charities as well investing in post-secondary education, Color Compass strives to give back to the communities in which we do business.</p>
+	</div>
+</div>
+<div class="row com-logos">
+	<div><a href="https://www.aiacanada.com/" target="_blank"><img src="/images/logos/aia.png" alt="AIA Canada" title="AIA Canada"></a></div>
+	<div><a href="http://www.ara.bc.ca/" target="_blank"><img src="/images/logos/ara-logo.png" alt="Automotive Retailers Association (ARA)" title="Automotive Retailers Association (ARA)"></a></div>
+	<div><a href="http://www.bcchildrens.ca/" target="_blank"><img src="/images/logos/BC Children’s Hospital.png" alt="BC Children’s Hospital" title="BC Children’s Hospital"></a></div>
+	<div><a href="http://www.ccif.ca/" target="_blank"><img src="/images/logos/ccif.png" alt="Canadian Collision Industry Forum" title="Canadian Collision Industry Forum"></a></div>
+	<div><a href="http://www.cancercare.mb.ca/" target="_blank"><img src="/images/logos/ccmb_logo.png" alt="Cancer Care Manitoba" title="Cancer Care Manitoba"></a></div>
+	<div><a href="http://www.cfff.ca/" target="_blank"><img src="/images/logos/Canadian-Fallen-Firefighters-Foundation.png" alt="Canadian Fallen Firefighters Foundation" title="Canadian Fallen Firefighters Foundation"></a></div>
+	<div><a href="http://www.cpalberta.com/" target="_blank"><img src="/images/logos/Cerebral Palsy.png" alt="Cerebral Palsy Association in Alberta" title="Cerebral Palsy Association in Alberta"></a></div>
+	<div><a href="http://www.child.ca/" target="_blank"><img src="/images/logos/Child-Foundation.png" alt="C.h.i.l.d. Foundation" title="C.h.i.l.d. Foundation"></a></div>
+	<div><a href="https://www.christmasbureau.ca/" target="_blank"><img src="/images/logos/Christmas Bureau.png" alt="Christmas Bureau of Edmonton" title="Christmas Bureau of Edmonton"></a></div>
+	<div><a href="http://www.cnc.bc.ca/" target="_blank"><img src="/images/logos/College of New Caledonia.png" alt="College of New Caledonia" title="College of New Caledonia"></a></div>
+	<div><a href="http://www.crohnsandcolitis.ca/" target="_blank"><img src="/images/logos/Crohns and Colitis Foundation.png" alt="Crohns and Colitis Foundation" title="Crohns and Colitis Foundation"></a></div>
+	<div><a href="http://www.cysticfibrosis.ca/" target="_blank"><img src="/images/logos/cfclogo-en.png" alt="Cystic Fibrosis Canada" title="Cystic Fibrosis Canada"></a></div>
+	<div><a href="http://www.deerlodge.mb.ca/" target="_blank"><img src="/images/logos/logoDLC_.png" alt="Deer Lodge Centre" title="Deer Lodge Centre"></a></div>
+	<div><a href="http://www.edmontonsfoodbank.com/" target="_blank"><img src="/images/logos/edmonton food bank.png" alt="Edmonton Food Bank" title="Edmonton Food Bank"></a></div>
+	<div><a href="http://epilepsysupport.ca/" target="_blank"><img src="/images/logos/Epilepsy Support Center.png" alt="Epilepsy Support Center" title="Epilepsy Support Center"></a></div>
+	<div><a href="https://www.lghfoundation.com/" target="_blank"><img src="/images/logos/Lions Gate.png" alt="Lions Gate Hospital Foundation" title="Lions Gate Hospital Foundation"></a></div>
+	<div><a href="http://www.lhsc.on.ca/" target="_blank"><img src="/images/logos/LHSC Logo.png" alt="Children’s Hospital London Health Sciences Centre" title="Children’s Hospital London Health Sciences Centre"></a></div>
+	<div><a href="https://makeawish.ca/" target="_blank"><img src="/images/logos/Make A Wish Canada.png" alt="Make A Wish Canada" title="Make A Wish Canada"></a></div>
+	<div><a href="http://www.okanagan.bc.ca/" target="_blank"><img src="/images/logos/Okanagan-College.png" alt="Okanagan College" title="Okanagan College"></a></div>
+	<div><a href="http://www.parkinsonalberta.ca/" target="_blank"><img src="/images/logos/Parkinson Alberta.png" alt="Parkinson Alberta" title="Parkinson Alberta"></a></div>
+	<div><a href="http://www.salvationarmy.ca/britishcolumbia/" target="_blank"><img src="/images/logos/Salvation Army British Columbia Division.png" alt="Salvation Army British Columbia Division" title="Salvation Army British Columbia Division"></a></div>
+	<div><a href="http://theseed.ca/" target="_blank"><img src="/images/logos/The Seed.png" alt="The Mustard Seed" title="The Mustard Seed"></a></div>
+	<div><a href="http://www.vcc.ca/" target="_blank"><img src="/images/logos/VCC.png" alt="Vancouver Community College" title="Vancouver Community College"></a></div>
+	<div><a href="https://www.younglife.ca/" target="_blank"><img src="/images/logos/Young Life.png" alt="Young Life of Canada" title="Young Life of Canada"></a></div>
+	<div><a href="http://www.redcross.ca/" target="_blank"><img src="/images/logos/canadian-red-cross.png" alt="Canadian Red Cross" title="Canadian Red Cross"></a></div>
+	<div><a href="http://www.shrinershospitalsforchildren.org/Locations/canada" target="_blank"><img src="/images/logos/Shriners.png" alt="Shriners Hospitals" title="Shriners Hospitals"></a></div>
+</div>
