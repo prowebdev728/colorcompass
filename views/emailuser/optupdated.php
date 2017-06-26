15d454f2c7d62662253eab7298c82229b7fcd7ae
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-26 08:30:29 +0800

    add

---
+++ views/emailuser/optupdated.php
@@ -0,0 +1,6 @@
+<div class="m-w1">
+	<div class="m-w2 section-inner">
+		<h2 class="clear">Subscription Updated</h2>
+		<p>Thank you. Your option has been updated. For more information please <a href="/emailuser/optinout">Click Here</a> to view our subscription page.</p>
+	</div>
+</div>
