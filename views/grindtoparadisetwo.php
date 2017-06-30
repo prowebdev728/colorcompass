4d6734a8978d5c9f74ec882095a542761bcd6854
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-30 18:39:29 +0800

    add

---
+++ views/grindtoparadisetwo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+		<title>Grid Your Way To Paradise Two Promo</title>
+		<link href="/css/normalize.css" rel="stylesheet" type="text/css">
+		<link href="/css/foundation.min.css" rel="stylesheet" type="text/css">
+	</head>
+	<body>
+		<div class="row">
+			<div class="column text-center" id="logo"><img src="/images/logo.png" alt="Color Compass Corporation" /></div>
+			<div class="small-12 column text-center"><img src="/images/3M-Promo2.jpg" alt="Grid Your Way To Paradise Two Promo" /></div>
+			<div class="small-12 column text-center"><h4><a href="grindtoparadisetwo/details">Full Contest Details Here</a></h4></div>
+		</div>
+	</body>
+</html>
