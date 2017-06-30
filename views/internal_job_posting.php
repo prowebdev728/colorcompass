1dbc1fde397561e1a337951770d1829e25a4861a
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-30 18:39:52 +0800

    add

---
+++ views/internal_job_posting.php
@@ -0,0 +1,40 @@
+<div class="row intro bpadding">
+	<img src="/images/generic-banner.jpg" alt="Internal Job Posting">
+</div>
+<div class="row column">
+	<h4>Internal Job Posting</h4>
+	<h5>Position: Junior IT/Kube Support Analyst</h5>
+	<div class="small-12 column">
+		<p><strong>RESPONSIBILITIES:</strong></p>
+		<ol>
+			<li>First level support, install, configure and deploy equipment to CCC employees: desktop PCs, laptops, desktop phones, iPhones, iPads, thin clients, printers, KUBE scanners.</li>
+			<li>First level support, install, configure and deploy site network equipment: switches, routers, printers, thin clients.</li>
+			<li>First level support, install, configure and deploy equipment to CCC customers: paint computers, KUBE scanners.</li>
+			<li>First level support for system issues related to E-mail, Windows updates, network, printing, VPN, antivirus.</li>
+			<li>Escalate support issues to the appropriate resource when needed.</li>
+			<li>First level technical support for KUBE back office.</li>
+			<li>Audit KUBE data integrity and maintenance and assist CCC and customer users with setup issues.</li>
+			<li>Assist with creating documentation for IT infrastructure configuration, troubleshooting and user interfaces.</li>
+			<li>Keep track of warranty expiration and renewal and/or equipment replacement.</li>
+			<li>Order and replenish printer supplies for all locations.</li>
+			<li>Personal PC purchase plan recommendations and procurement.</li>
+			<li>Phone System maintenance and support.</li>
+		</ol>
+	</div>
+	<div class="small-12 column">
+		<p><strong>REQUIRED SKILLS:</strong></p>
+		<ol>
+			<li>Windows Networking training is a must.</li>
+			<li>Setup and configuration knowledge of Networking: TCP/IP, SMTP, DNS, DHCP, VPN, security, firewalls, antivirus.</li>
+			<li>Setup and configuration knowledge of Windows OS: security, networking, printing, remote connectivity, backup and restore, recovery, windows updates.</li>
+			<li>User knowledge of iOS</li>
+			<li>Problem solving skills.</li>
+			<li>Learns quickly and adapts rapidly to change.</li>
+			<li>Excellent communication skills.</li>
+			<li>Team player.</li>
+		</ol>
+	</div>
+	<p class="samll-12 column">
+		Contact Carmen Stanciulescu <a href="mailto: cstanciulescu@colorcompass.com">cstanciulescu@colorcompass.com</a> for More Details or to Apply!
+	</p>
+</div>
