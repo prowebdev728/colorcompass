569f4a8779839581219217c81626021a7f85e044
prowebdev728 <proweb.developer2016@yandex.com>
2017-06-30 18:39:47 +0800

    add

--- views/index.php
+++ views/index.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="utf-8"/>
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Color Compass</title>
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300|Roboto+Condensed" rel="stylesheet">
 	<link href='https://fonts.googleapis.com/css?family=Ramabhadra' rel='stylesheet'>
