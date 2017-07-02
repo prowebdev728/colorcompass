545b339627ff746d3a23abd76ba9b387b7ae81f7
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-03 00:55:37 +0800

    add

---
+++ views/news.php
@@ -0,0 +1,11 @@
+		<div class="row intro bpadding">
+			<img src="/images/news-banner.jpg">
+		</div>
+		<div class="row">
+			<div class="small-12 medium-12 column">
+				<h3>Color Compass News</h3>
+				<?=$new_news;?>
+				<h4>Color Compass Past News</h4>
+				<?=$old_news;?>
+			</div>
+		</div>
