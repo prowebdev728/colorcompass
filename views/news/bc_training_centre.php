7b91cc5be8c74153bf9a7027456abba6f3436427
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-03 00:55:56 +0800

    add

---
+++ views/news/bc_training_centre.php
@@ -0,0 +1,26 @@
+<div class="news">
+	<h5>Color Compass BC Training Centre</h5>
+	<p>The new BC  Color Compass training centre is taking shape at the Coquitlam office. Once fully completed the new centre will bring the total number to four facilities for the Color Compass group throughout Western Canada, which is beyond market leading!</p>
+	<div class="image">
+		<img src="/images/news/bc-training-centre.jpg" alt="BC Training Centre">
+	</div>
+	<p>The show-stopper in the 3,400 square foot technical area complete with a Global Ultra advanced down draft spray booth that has been installed with some beautiful duct work and metal fabrication. Complementing the spray booth  is the Nitro Heat spray system, hip blowers, 15000 CFM AMU , and an 8 x8 prep deck with 6000 cfm for test panels and smaller training scenarios. We will also  have a Car-o-Liner Quick 42 mobile rack, Pro Spot I5 resistance welder, Pro Spot SP- 5 Multi welder, Wedge Clamp Eclipse measuring system, and the Wedge Clamp Nitro Plastic Nitrogen Welder.</p>
+	<p>The second stage of the project is the 1,500 square foot state of the art classroom where, if you're familiar with the Coquitlam building, is taking shape where the PBE and Marketing offices once were. The classroom will have full audio and visual capabilities to enable a more interactive training environment. With a total capacity north of 30 individuals the Color Compass University classroom will be able to handle larger training session, internal and external meetings, and other industry events.</p>
+	<p>As the centre's official opening is fast approaching the timing couldn't be better for Color Compass and the collision repair industry. As the complexity of vehicle repairs increase so must the equipment and training for that equipment, with this opening we will be positioned to facilitate and offer top-tier industry training in our four major markets in Western Canada.</p>
+	<p>Training begins March 20th with a BETAG session, the official opening is slated for April 4th!</p>
+	<div class="row small-up-2 medium-up-3 padded-top">
+		<div class="column padded-vertical"><img src="/images/news/bc-training-1.jpg" alt="BC Training Center 1"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-2.jpg" alt="BC Training Center 2"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-3.jpg" alt="BC Training Center 3"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-4.jpg" alt="BC Training Center 4"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-5.jpg" alt="BC Training Center 5"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-8.jpg" alt="BC Training Center 8"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-9.jpg" alt="BC Training Center 9"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-33.jpg" alt="BC Training Center 33"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-44.jpg" alt="BC Training Center 44"></div>
+	</div>
+	<div class="row small-up-2 medium-up-3 padded-top">
+		<div class="column padded-vertical"><img src="/images/news/bc-training-6.jpg" alt="BC Training Center 6"></div>
+		<div class="column padded-vertical"><img src="/images/news/bc-training-7.jpg" alt="BC Training Center 7"></div>
+	</div>
+</div>
