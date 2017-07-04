d71357fac091d1ebbe9d257115fda34e4802df6a
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-05 04:58:58 +0800

    add

---
+++ views/news/cadillacs_new_book.php
@@ -0,0 +1,55 @@
+<div class="news">
+	<h5>Cadillac's New "Book" Service Could Be A Game Changer</h5>
+	<p>The following is a firsthand look at one users experience with Cadillac's new Book program that they are currently market testing in the New York area.</p>
+	<p>General Motors is making it clear that it doesn't just want to be known as a carmaker.</p>
+	<p>During 2016 alone, the auto giant invested in a number of mobility services including a $500 million investment in the ride-hailing service Lyft and launched its own ride-sharing service called Maven.</p>
+	<p>The company's latest move, though, is one of its most forward-thinking endeavors.</p>
+	<p>In January, GM's Cadillac brand announced the beta launch of its new Book service, which is a subscription-based concierge service for all Cadillac cars. Users pay a flat fee and have access to whatever Cadillac car they want, whenever they want.</p>
+	<p>Because it's a beta launch, the service is only available in the New York City region, but the company has plans to expand to more US cities in the future.</p>
+	<p>We recently had the chance to try out Book for a few weeks and quickly embraced the subscription model. However, given it is still a beta version of the service, we did find a few things that could use improvement.</p>
+	<p>Here's how Cadillac's premium subscription service works and what it's like to use.</p>
+	<p>First, Cadillac's Book service is not cheap. It's $1,500 a month plus a one-time initiation fee of $500. The pricey service automatically narrows the pool of who can sign up for the service.</p>
+	<p>Members can cancel at any time and they can also reinstate their subscription at any time without paying any fees.</p>
+	<p>That being said, the Book service actually comes with quite a bit included so if you have the extra cash, you may be able to justify the cost. Also, there's a chance that the company could revise its pricing structure.</p>
+	<p>The Book service includes 18 car exchanges within 12 months, unlimited mileage, insurance, and registration costs are included, and you never have to worry about car maintenance.</p>
+	<p>Cadillac's brand marketing director Melody Lee told Business Insider that they decided on $1,500 because it seemed liked a competitive price when considering all that is included with the service.</p>
+	<p>"We looked at a lot of different elements in order to come up with a price point that was both indicative of the premium experience you would get with Book, as well as something that we thought was compelling," Lee said.</p>
+	<p>"And when you really look at the $1,500 number and you consider insurance and registration, maintenance, detailing, a lot of things that you traditionally have to worry about separately are all just rolled into this all in one price, I think it becomes a very compelling thing."</p>
+	<p>However, Lee said that Cadillac could change the pricing structure after the program is out of beta and may even consider a tiered subscription service.</p>
+	<p>Price aside, the Book experience is quite impressive. It's all app-based and incredibly intuitive.</p>
+	<img src="/images/news/cadillac-book1.jpg" alt="Cadillac Book 1">
+	<p>To enroll in the Book service, you must first sign up online. Once you are accepted into the program, you simply download the app and make an account.</p>
+	<p>Cadillac developed the app in-house and plans to keep improving the app's user interface via software updates.</p>
+	<p>"We are going to continue to make the experience better and better," Lee said. "In the next phase, expect to see even greater improvements for our users. We want to make it even more intuitive and for the UI to be extremely simple and easy to use."</p>
+	<p>Once logged into the app, the user is presented with a selection of current-year platinum-level-trim Cadillacs, including the XT5, CT6, Escalade, and V Series.</p>
+	<img src="/images/news/cadillac-book2.jpg" alt="Cadillac Book 2">
+	<p>You can also use the filter feature so that only a certain kind of car is displayed. For example, you can filter it so that only SUVs are shown or only sedans.</p>
+	<p>From here, a user simply selects which car he or she wants, the drop-off and pick-up date, and the time and place he or she wants the car delivered.</p>
+	<img src="/images/news/cadillac-book3.jpg" alt="Cadillac Book 3">
+	<p>The concierge will pick up and deliver to all five boroughs and to some cities in New Jersey.</p>
+	<p>The first car I checked out was the Cadillac CT6. I requested the concierge drop the vehicle off on 5th Avenue.</p>
+	<img src="/images/news/cadillac-book4.jpg" alt="Cadillac Book 4">
+	<p>It may not be the most powerful car on the menu, but its tech features make it my personal favorite Cadillac car.</p>
+	<p>When the concierge drops the car off, he offered to walk me through the car's features and asked if I had any questions.</p>
+	<p>Having driven the CT6 before, I opted to skip the walk-through, gave my thanks, and took the keys. But if you are unfamiliar with the vehicle, the concierge will take a few minutes to highlight some of the vehicle's best features. </p>
+	<p>The first time you check out a car, you receive a swag bag which contains a Cadillac branded umbrella and notepad.</p>
+	<img src="/images/news/cadillac-book5.jpg" alt="Cadillac Book 5">
+	<p>All vehicles in the program have access to On-Star services, Sirius XM, and an unlimited 4G LTE + Wi-Fi Hotspot.</p>
+	<p>Every car comes stocked with bottled water and mints, which is a nice touch.</p>
+	<p>In addition to the CT6, I also checked out the Cadillac ATS-V and ...</p>
+	<img src="/images/news/cadillac-book6.jpg" alt="Cadillac Book 6">
+	<p>The Cadillac ATS-V is one of the performance cars the brand offers via Book</p>
+	<p>Overall, using Book was an enjoyable experience. However, there was one issue that kept coming up: parking.</p>
+	<p>A $1,500 car subscription should come with some parking options.</p>
+	<p>Currently, the Book program offers a lot. </p>
+	<p>You can have a premium-trim-level Cadillac whenever you want without the usual hassles of buying or leasing a car. Sure, it's expensive, but Book's convenience may be worth it if you can afford it.</p>
+	<p>But, there's one problem Cadillac hasn't fixed just yet and that's the parking conundrum.</p>
+	<p>You see, once you've checked a car out, you have to keep that car until you are ready to exchange it for another vehicle. In other words, there is a vehicle in your position at all times, even when you don't want it. </p>
+	<p>This means you have to deal with parking, and parking in New York City can be a nightmare. On several occasions, I found myself driving in circles looking for either a parking spot or a garage. And the few times I used a garage, I ended up spending a small fortune. </p>
+	<p>For $1,500 a month, it would be great if Cadillac included some sort of option where users could return the car after using it without having to check out another vehicle so that members don't have to always deal with parking.</p>
+	<p>That being said, the service is still in beta and Cadillac is looking at new ways to improve the service and address the parking issue, Lee said. </p>
+	<p>"There are so many interesting, entrepreneurial ways to approach parking. Whether it's pointing you towards the nearest parking structure or partnering with really interesting companies...that do valet parking for you wherever you go, there are a lot of ways we could do it," Lee said.</p>
+	<p>"So those things are absolutely top of mind and things that we are working towards in the next iteration of Book."</p>
+	<p>- Business Insider</p>
+	<p>Will this approach to car ownership (or lack of ownership) change the game, will other manufactureres tread down the same path as competition from ride-sharing becomes even more fierece? Time will tell.</p>
+</div>
