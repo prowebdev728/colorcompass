3b63088d107dd6e00b72c887aa9acf8994bacac8
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-05 04:59:06 +0800

    add

---
+++ views/news/carlson_alberta_car_event.php
@@ -0,0 +1,32 @@
+<div class="news">
+	<h5>Carlson A Major Sponsor at Renowned Alberta Car Event</h5>
+	<div class="image text-center">
+		<img src="/images/news/carlson-sponsor4.jpg" alt="Carlson A Major Sponsor">
+	</div>
+	<p>The North American love affair with the automobile is legendary. In fact, for over fifty-seven years, custom car shows have paid tribute to the innovative workmanship that continuously re-invents and improves the vehicles of yesterday and today. Championship Auto Shows (CASI), the undisputed leader in indoor custom car show production, produces the largest show series in the business: better known under the monikers of Autorama or World of Wheels. Close to 20 events are held annually across the United States and Canada.</p>
+	<p>Hot rods are always at the heart of each show, but in recent years the composition has expanded to include manufacturers' displays. As a result, today's shows appeal equally to those who own competition cars, specialty and concept vehicles and those who dream about owning them.</p>
+	<p>The two World of Wheels events in Alberta, the Edmonton event presented by Carlson,  provided a chance to check out some of Western Canada's finest hot rods, custom cars, restored motorcycles and trucks. In addition, at the Calgary event attendees got a chance to witness the annual Pedal Car Challenge, part of Student Career Day sponsored by Carlson. </p>
+	<p>The Pedal Car challenge provides an excellent opportunity for a team of automotive students from local high schools to apply their technical skills. The hands-on projects involve planning, design, creativity, productivity and resourcefulness. In other words, it's a great starting point for a career in collision repair.</p>
+	<p>"It is an outstanding event and we were thrilled to be part of Student Career Day again this year," says Jamie Corbeil, Southern Alberta Sales Manager for Carlson Body Shop Supply. "It’s exciting to see the students so engaged through their pedals car designs. We hope events like this will continue to attract the next generation of skilled technicians into the collision repair industry!"</p>
+	<p>Participating schools were provided with one pedal car to customize and show off at the event. Student Career day kicked off with a general discussion in the auditorium with over 500 students in attendance, with guest speakers from many sectors of the automotive industry. Student Day is designed to help encourage students to follow their passion for the automobile and consider careers in the automotive industry.</p>
+	<p>Carlson Body Shop Supply also contributed paint and body shop supplies, in parts from funds raised during the Chip Foose silent auction the company held in May 2016 at the Calgary Color Compass Training Centre. </p>
+	<p>As part of the Pedal Car Challenge, each participating high school received a package of materials which enabled them to follow the typical collision repair process from metal work, to sanding and preparation to final paint procedures. BASF provided the schools with the colours they would need to express their creativity. The technical sales team from Carlson Body Shop Supply assisted the schools with product application and procedures.</p>
+	<p>Aside from the participation at the Student Career Day and being the presenting sponsor in Edmonton, Carlson also had a presence on the show floor with two outstanding looking booths in both Edmonton and Calgary. Those who stopped by got the chance to see a mini Carlson storefront, some eye-catching muscle cars, and  the new show stopping Zrylliant paint products from BASF. Carlson GM Dave Swenson was even interviewed by City TV in Edmonton, Hollywood may come calling Dave!</p>
+	<div class="text-center">
+		<iframe width="560" height="315" src="https://www.youtube.com/embed/gSlck-rjpQQ" frameborder="0" allowfullscreen></iframe>
+	</div>
+	<p>Special thank you to all of the Carlson and BASF staff who devoted their time in Calgary and Edmonton to make these shows a success!</p>
+		<div class="row small-up-1 medium-up-4">
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor1.jpg" alt="Carlson Sponsor 1"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor2.jpg" alt="Carlson Sponsor 2"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor3.jpg" alt="Carlson Sponsor 3"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor5.jpg" alt="Carlson Sponsor 5"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor6.jpg" alt="Carlson Sponsor 6"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor7.jpg" alt="Carlson Sponsor 7"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor8.jpg" alt="Carlson Sponsor 8"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor9.jpg" alt="Carlson Sponsor 9"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor10.jpg" alt="Carlson Sponsor 10"></div>
+		<div class="column padded-vertical"><img src="/images/news/carlson-sponsor11.jpg" alt="Carlson Sponsor 11"></div>
+		</div>
+
+</div>
