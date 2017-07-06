0cfd8f8e7e879d27943197a0bcdbfcd3e83e1812
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-06 21:31:26 +0800

    add

---
+++ views/news/chase_holds_3m_product.php
@@ -0,0 +1,22 @@
+<div class="news">
+	<h5>Chase Brandon Holds 3M Product Night</h5>
+	<div class="image text-center">
+		<img src="/images/news/chase-brandon.jpg" alt="Chase Brandon Holds 3M Product Night">
+	</div>
+	<p>On Tuesday April 18th the team at Chase Brandon partnered with 3M and proved that small town branches can make a big impact in their market. The catered event was held in the evening and featured all of the new products that 3M has recently released. From their latest sandpaper release to their new power tools and vacuum sanding system attendees had the chance to see it all. The Festool 3M Sanding system was actually the featured highlight of the evening, and this just wasn't your typical show and tell, Brett MacNeil from 3M fired up the unit and had the attendees get a little "dust-less" for a full live demonstration. The evening also included door prizes for some lucky attendees; Rick Gray (Kelleher Ford) won a Winnipeg Blue Bomber Helmet, and Drake Watson (Watson Autobody) walked away with a new LCD TV. </p>
+	<p>Perry Grift, Chase Brandon Market Manager, summed up the vision behind the night. "We wanted to invite customers in to showcase & demo some new products 3M has recently introduced.  We also thought it would be a great opportunity to give a refresher on other 3M products which they use on a daily basis.  We were pleased with the turnout and the customers really seemed to enjoy the evening."</p>
+	<p>With the rapid pace of change in the industry Chase Brandon hopes to hold more evenings like the successful 3M night, hopefully expanding with other suppliers and manufacturers.</p>
+	<div class="row small-up-2 medium-up-4 padded-top">
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon1.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon2.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon3.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon4.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+	</div>
+	<div class="row small-up-2 medium-up-4 padded-top">
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon5.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon6.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon8.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon9.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-brandon10.jpg" alt="Chase Brandon Holds 3M Product Night"></div>
+	</div>
+</div>
