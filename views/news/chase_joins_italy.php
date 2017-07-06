6d9a4310f28a3f508c424d1c89155e635111d97a
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-06 21:31:38 +0800

    add

---
+++ views/news/chase_joins_italy.php
@@ -0,0 +1,45 @@
+<div class="news">
+	<h5>Chase Joins Customers &amp; Suppliers in Italy</h5>
+	<div class="image">
+		<img src="/images/news/chase-italy.jpg" alt="Chase Joins Customers & Suppliers in Italy">
+	</div>
+	<p>Recently John Santos, Business Development Manager, and Justin Zelinski, Sales Representative with Chase joined customers and vendors on a very special trip to Italy. The crux of the trip was to visit Symach, a revolutionary equipment production system for collision repair facilities. We asked John a little bit about the trip, and in true John fashion, he took us on a first-class tour!</p>
+	<p>Imagine having the opportunity to check three things off your bucket list…going to Italy, touring through Ferrari and Lamborghini and seeing the statue of David. I was thrilled to be given the opportunity to represent Color Compass and Chase Autobody Supplies and accompany Chase customers to the Symach facility in Bologna Italy. Jenn Giocoli from Precision Auto Body in Saskatoon was the person who originally got the ball rolling. She had suggested that AkzoNobel coordinate an opportunity for customers to visit the facility. Our friends at Akzo worked with the team at Symach to put a great week together for us all.</p>
+	<p>Chase had a good contingent of customers in attendance. Joe and Jenn Giocoli from Precision Auto Body in Saskatoon, Peter and Efaf Fasnik from Kelowna Performance and Mervyns in Kelowna, Dan and Samantha Laird from BC Collision in Burnaby, Joel McPhail from Craig Dunn GM located in Portage La Prairie and Adrian Vander Graaf from EK Collision Repair Centre Ltd. in Winnipeg. Representing Chase and Color Compass was Justin Zelinski and I.</p>
+	<p>There were several highlights of the trip and the week was packed full of adventure. We began the week with a visit to the Symach training center and production plant. There we were met by the Symach team. Leading the team is Osvaldo Bergaglio. Osvaldo is the owner and creator of the revolutionary Symach Fixline Process. The process is aimed at increasing production with fewer techs. This I had to see especially given the tech shortage our industry faces.  According to Osvaldo, a painter is potentially able to complete 10 jobs per day! According to the people at Symach, the total number of technicians needed to complete the same amount of repairs as a conventional facility is reduced by approximately 30%. Leveraging the Drytonic technology is what enables them to achieve these production advantages as the Drytonic technology dries filler, primer, waterbase and clear in less than 2 minutes per panel length. The system boasts an average key to key time of two days. That said, purchasing the Fixline process comes at a price…Osvaldo says the ROI is normally around 18 months. I think one of the questions one needs to ask themselves, when considering investing in a system like Fixline, is does the facility have the work to feed the beast?  With insurance companies looking for shorter cycle times and our industry dealing with the challenge of finding qualified techs, the Symach Fixline Process certainly seems equipped to assist with these challenges.  Peter and Efaf Fasnik are going to put Symach’s production advantages to the test, as they have decided to install the Symach Fixline System in their new facility in Kelowna, making it the first in British Columbia.</p>
+	<p>During the week we also had the pleasure of visiting both the Ferrari and Lamborghini museums and were fortunate enough to have a guided tour through the Lamborghini production plant…definitely one of the highlights of the trip. In the small amount of down time we had we enjoyed some amazing food and wine and saw some incredible history. We had the pleasure of seeing some of Italy’s history wandering the town of Brescia, where we walked into a Church with beautiful art covering its ceiling.  As an art lover, going to Florence and seeing some of the incredible architecture and the statue of David has to go down as one the most amazing art experiences I have ever had. Bologna is also known for creating the famous Bolognese Ragu pasta dish. Equipped with this information I felt it was my duty to enjoy this and many other pasta dishes washed down with a couple of glasses of claret whenever possible!</p>
+	<p>The trip was an amazing experience enjoyed with some great customers and business partners.  Thanks go out to Jenn for coming up with the idea and for AkzoNobel and Symach for making it happen. Special thanks to Chase and Color Compass for helping me check a few things of my bucket list!</p>
+	<p>Arrivederci</p>
+	<p>John Santos</p>
+	<div class="row small-up-2 medium-up-4 padded-top">
+		<div class="column padded-vertical"><img src="/images/news/chase-italy1.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy2.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy3.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy4.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy5.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy6.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy7.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy9.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy11.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy12.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy13.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy14.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy15.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy16.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy18.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy19.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy22.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy23.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy25.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy26.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy27.jpg" alt="Chase Italy"></div>
+	</div>
+	<div class="row small-up-2 medium-up-4 padded-top">
+		<div class="column padded-vertical"><img src="/images/news/chase-italy8.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy10.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy17.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy20.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy21.jpg" alt="Chase Italy"></div>
+		<div class="column padded-vertical"><img src="/images/news/chase-italy24.jpg" alt="Chase Italy"></div>
+	</div>
+</div>
