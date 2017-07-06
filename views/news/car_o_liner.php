18eb6465eb11fdd08eb71a6b850ffc41c361a256
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-06 21:31:07 +0800

    add

---
+++ views/news/car_o_liner.php
@@ -0,0 +1,10 @@
+<div class="news">
+	<h5>New Product Line</h5>
+	<div class="text-center padded">
+		<img src="/images/news/car-o-liner-1.png" alt="Car-O-Liner">
+	</div>
+	<p>Car-O-Liner has announced that Color Compass Corporation is now distributing the company's equipment in British Columbia.</p>
+	<p>"We are extremely excited to be working with Car-O-Liner Company and look forward to a great partnership", said Mort Hall, General Manager of Color Compass Corporation. "Car-O-Liner gives us an exciting and respected product line to offer to our clientele which in turn will only make them more efficient and profitable."</p>
+	<p>Car-O-Liner manufactures solutions and equipment for repair and alignment, including frame racks and measuring systems.</p>
+	<p>"Car-O-Liner is very proud to partner with Color Compass and we see great opportunity to serve our customers better as we continue to grow at a rapid pace," said Doug Bortz Director of Sales & Marketing for Car-O-Liner. "Color Compass is a well-respected supplier for much of Canada and they came highly recommended directly from their customers. We are excited to work with Mort Hall, Tim Shay and Color Compass in British Columbia."</p>
+</div>
