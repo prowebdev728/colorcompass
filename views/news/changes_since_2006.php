448df01584339c67fd6e4d436819b006d369e02f
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-06 21:31:14 +0800

    add

---
+++ views/news/changes_since_2006.php
@@ -0,0 +1,57 @@
+<div class="news">
+	<h5>A Lot Has Changed Since 2006</h5>
+	<div class="image">
+		<img src="/images/news/changes-2006.jpg" alt="A Lot Has Changed Since 2006">
+	</div>
+	<p>The drought is finally over for Oilers fans, and while the editors here at the Connected must stay impartial (the Canucks will rise again!), we thought it would be interesting to look back at what has happened since the Oil were last playing playoff games in 2006.</p>
+	<p>For starters, no one had an iPhone back then (Blackberry was king). The original version of the Apple smartphone was not released until June 29, 2007. The population of the City of Champions has jumped by more than 200,000 people from 730,372 in 2006 to 932,546 in 2016. And Oilers star center Connor McDavid was just nine years old.</p>
+	<p>Here are a few other things that have changed since 2006.</p>
+	<dl>
+		<dt>Social media</dt>
+		<dd>
+			<p>Hockey fans love taking to Twitter during games, but not a single person has tweeted during an Oilers playoff game. That's because Twitter didn't launch until July 2006, after the team's '06 Cup run was over.</p>
+			<p>The Oilers signed up for Twitter in July 2008 and sent their first tweet on March 7, 2009.</p>
+			<p>And while Facebook was available to Harvard students in 2004, it wasn't until September 2006 that the social network was extended beyond educational institutions to anyone with a registered email address.</p>
+			<p>YouTube was in its infancy the last time the Oilers made the post-season. Founded in February 2005, YouTube was bought by Google in November 2006 for US$1.65 billion.</p>
+		</dd>
+		<dt>Oilers team</dt>
+		<dd>
+			<p>By the time the Oilers start the post-season, they will have played 868 regular-season games between playoff appearances. To put that in perspective, former Oilers goaltender Grant Fuhr played 867 career regular-season games. And in the first 868 regular-season games in Oilers history, the team won four Stanley Cups and were en route to winning their fifth. When it comes to the current team, it's evenly split between players who have played in an NHL post-season game and players who haven't. Oilers veteran Jordan Eberle, who played his 500th NHL game on Saturday, has never seen the playoffs. Until now.</p>
+			<p>The head coach at the time, Craig MacTavish, was canned by the team in 2009 after missing the playoffs for the third straight year. But he wasn't gone long, as he was hired back by the Oilers in 2012 and is currently the Oilers vice president of Hockey Operations.</p>
+			<p>After MacTavish was fired as head coach, the team went through five head coaches before landing on current head coach Todd McLellan.</p>
+			<p>The ownership of the team has also changed hands since 2006. Daryl Katz bought the team from the Edmonton Investors Group in 2008.</p>
+		</dd>
+		<dt>Rocket visits Pluto</dt>
+		<dd>
+			<p>In the time between Oilers' playoff visits, a rocket made its way 7.5 billion kilometres from Earth to Pluto. NASA's New Horizons spacecraft launched from Cape Canaveral Air Force Station in Florida on Jan. 19, 2006. The spacecraft flew by the dwarf planet in the summer of 2015.</p>
+		</dd>
+		<dt>Hollywood</dt>
+		<dd>
+			<p>Tom Cruise and Katie Holmes were one of the hottest couples of the time and on Nov. 18, 2006 tied the knot at Odescalchi Castle in Bracciano, Italy. The couple's marriage didn't last as long as the Oilers' playoff drought, though, as Holmes filed for divorce in 2012. Crash won the Oscar for best picture in 2006, beating out Brokeback Mountain, Capote, Good Night and Good Luck and Munich. Disney's Pirates of the Caribbean: Dead Man's Chest was the highest-grossing film of the year, and Borat visited the US from Kazakhstan, enough said.  Daniel Powter’s Bad Day was the top song of 2006, according to Billboard. Hannah Montana, starring a then 13-year-old Miley Cyrus, also premiered in March 2006.</p>
+		</dd>
+		<dt>Politics</dt>
+		<dd>
+			<p>Alberta has gone through six premiers since the last time the Oilers were in the playoffs. During the '06 Cup run, the late Ralph Klein was premier. Since then, the province's top spot has been held by Ed Stelmach, Alison Redford, Dave Hancock, Jim Prentice and now Rachel Notley. Federally, Stephen Harper was the new PM on the block, and started what would become a nine year tenure.</p>
+		</dd>
+		<dt>Cars</dt>
+		<dd>
+			<p>Although exterior design and components have changed, as well as interior refinement and technology, the top-selling cars of 2006 are strikingly similar to the top-selling cars of today.</p>
+			<ul>
+				<li>Ford F-Series (includes F-150, F-250 Super Duty and F-350 Super Duty)</li>
+				<li>Chevrolet Silverado1.Chevrolet Silverado (includes 1500, 1500 Classic, 1500 SS Classic, 1500HD Classic, 2500HD, 2500HD Classic, 3500HD, 3500 Classic</li>
+				<li>Toyota Camry</li>
+				<li>Dodge Ram (includes 1500, 2500 and 3500)</li>
+				<li>Honda Accord</li>
+				<li>Toyota Corolla</li>
+				<li>Honda Civic</li>
+				<li>Chevrolet Impala</li>
+				<li>Nissan Altima</li>
+				<li>Chevrolet Cobalt</li>
+			</ul>
+		</dd>
+		<dt>Color Compass</dt>
+		<dd>
+			<p>In 2006 Color Compass had just acquired Valley Car Color in British Columbia, and was still distributing good old fashioned solvent-borne paint systems, the Canadian Federal (VOC) limit regulations had not yet taken effect. Rocky Mountain Sales in Edmonton was still not a part of the Color Compass group, and Chase Auto Body Supplies was only operating in Manitoba and Saskatchewan.</p>
+		</dd>
+	</dl>
+</div>
