4a7905c65491cc1ae44a6cb678a6b508e74d3a52
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-07 09:23:13 +0800

    add

---
+++ views/news/chase_wins_distributor_leadership_award.php
@@ -0,0 +1,19 @@
+<div class="news">
+	<h5>Chase Wins Distributor Leadership Award</h5>
+	<p>Sarasota, Florida – October 18th, 2016 – At the annual AkzoNobel Distributor Conference in Sarasota Florida Chase Auto Body Supplies was presented with the prestigious AkzoNobel Distributor Leadership Award.</p>
+	<p>This award is given to the distributor within the entire Canadian Distribution network that contributes in communication, and working with AkzoNobel through cooperative alignment to provide more overall value for their customers.  The award is presented to the distributor that drives innovation, leverages tools and resources in providing excellent customer service, and has a keen focus of reinvesting back into their business. They are active in industry charities and focused on sustainable (long-term) sales growth through collaborative market planning in the markets they serve.</p>
+	<div class="image">
+		<img src="/images/news/chase-leadership-award.jpg" alt="Chase Leadership Award">
+	</div>
+	<p>Why Chase was selected</p>
+	<ul>
+		<li>Chase has been an AkzoNobel distributor for over 32 years (1984) beginning with humble roots in Winnipeg, Manitoba. Chase is very active in all Industry, network and charitable events that keep the market up to date and current.</li>
+		<li>Chase has a very sustainable business model, they are investing back in their people and have built a University training program. Along with training, they have three existing training facilities they currently operate and are opening a fourth in Vancouver, BC in 2017.</li>
+		<li>They invest in Cutting Edge end-user Automated Inventory system (KUBE) that will assist end-users in minimizing stock levels and predictability.</li>
+		<li>Chase employees support and lead ICAR training programs in Western Canada</li>
+		<li>Chase proactively encourage and implement Mutual Action Plans with each of their large MVC/MSO in market.</li>
+		<li>They recognized the demand for more lean processes and have put a structure in place to support PCE procedures. Along with AkzoNobel Services consultants they support the AkzoNobel RPG’s and PCE value-add to end-users.</li>
+		<li>It isn’t always about the service side of the business; they have maintained volume and grew sales regardless of recent economic struggles in Western Canada.</li>
+	</ul>
+	<p>Tim Shay,  General Manager of Chase Auto Body Supplies was on hand to accept the award on behalf of the great Chase team throughout Manitoba, Saskatchewan, Alberta, and British Columbia. </p>
+</div>
