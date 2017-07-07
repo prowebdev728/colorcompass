7e762a4a0379c3722546131ff77817507b0c63c8
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-07 09:23:20 +0800

    add

---
+++ views/news/color_compass_first_charity_golf_tournament.php
@@ -0,0 +1,11 @@
+<div class="news">
+	<h5>PBE &amp; Color Compass Host Two Charity Golf Events</h5>
+	<div class="image">
+		<img src="/images/news/peter-lui.jpg" alt="Peter Lui">
+		<small>Peter Lui, Sales Representative with White &amp; Peters and his spotter try to track down Peter's usually big drive</small>
+	</div>
+	<p>
+	On September 8th Color Compass and PBE Distributors held its first charity golf tournament of the year at the beautiful Morgan Creek Golf Course in Surrey, British Columbia. All proceeds from the tournament go to support Color Compass' charity of choice in BC, the BC Children's Hospital. The day started out with your typical west coast late summer weather, but eventually cleared up and the golfers were treated to a beautiful, sunny day. Color Compass staff and customers were joined by representatives of vendor partners and other industry stakeholders, with a total of 160 golfers participating in the 24th annual tournament. Two short weeks later, on September 21st, PBE and Color Compass again hosted its annual Eastern Charity Golf Classic at Glencairn Golf Club in Halton Hills, Ont. This year's tournament was the 20th anniversary of the event, which drew stakeholders from across a number of segments of the auto repair industry. All proceeds from the Eastern Charity Golf Classic go to benefit  the Children’s Hospital of Western Ontario at London Health Sciences. In addition to a great round of golf using Texas Scramble rules, the day also included a BBQ lunch sponsored by BASF and a dinner followed by awards, prizes and 50/50 cash draws.
+	<br><br>Sponsors For Both Events were 3M, BASF, Saint Gobain, Valspar, Pro Form and AkzoNobel.
+	</p>
+</div>
