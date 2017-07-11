dde9224ba77224cb1a8afde6aaf8da81bf5abecc
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-11 17:18:33 +0800

    add

---
+++ views/news/color_compass_wins_distributor_year_2015.php
@@ -0,0 +1,16 @@
+<div class="news">
+	<h5>Color Compass Wins Distributor of the Year 2015</h5>
+	<p>Vancouver, British Columbia -- Color Compass Corporation has received the Pro Spot International Distributor of the Year award, presented at the 2015 SEMA Show. In essence, this award is given to the distributor who sold the most Pro Spot product in the last year, but there’s much more to it than that.</p>
+	<div class="image">
+		<img src="/images/news/news1.jpg" alt="jack">
+		<small>Russell Duncan (center), Color Compass Corporation's Equipment Specialist, accepting the award from Art Ewing of Pro Spot Canada and Ron Olsson, President of Pro Spot International.</small>
+	</div>
+	<p>&quot;The primary driver is your sales, but it also takes into consideration the support mechanism that follows the sale," says Russell Duncan, an equipment specialist with Color Compass Corporation who accepted the award. "They look into the training you provide and the service level you provide to your customer base. With every unit we sold, we were installing, training and offering service throughout. Rather than selling the unit and letting the customer deal with whatever came up, we really made sure that we were on hand to help them find success.&quot;</p>
+	<p>Color Compass Corporation's win becomes even more impressive once you know that. The sales figures alone are impressive, when you consider that Color Compass Corporation was being measured against large organizations in the US where the market is approximately ten times the size. How did they do it?</p>
+	<p>First, the company put a dedicated person in charge of the program. For the last year, Duncan’s sole responsibility was welding and certification of collision repair facilities. Second, they didn’t limit their calls to current Color Compass customers.</p>
+	<p>&quot;We were offering a complete consultative process to every shop and MSO, not just our paint customers,&quot; says Duncan. &quot; we weren't coming in to threaten existing paint business, we received a lot of cooperation.&quot;</p>
+	<p>There's a third piece to the puzzle as well. Duncan also stresses that it wasn’t simply about making the sale, but about really consulting with the collision facility and determining what was needed in each case.</p>
+	<p>&quot;We weren’t going in and saying 'Here’s a welding package!' or 'Here’s an aluminum package!' We really look at what the customer already has and what they really need. In some cases, they already had equipment that they could use for some operations, so we would sell them what they needed to fill the gaps,&quot; he says.</p>
+	<p>Putting their customers’ interests first paid off. Duncan says that word spread quickly, and soon he and his team became the go-to people for welding in western Canada. Color Compass Corporation intends to bring the same methods to Ontario next, through its subsidiary PBE Distributors.</p>
+	<p><em>Courtesy of Collision Repair Magazine</em></p>
+</div>
