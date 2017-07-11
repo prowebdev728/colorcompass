0d82c64b676d0026a626c1dff2ba1c5f273a67f2
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-11 17:18:52 +0800

    add

---
+++ views/news/exciting_changes_happened_calgary.php
@@ -0,0 +1,31 @@
+<div class="news">
+	<h5>Exciting Changes Happened In Calgary!</h5>
+	<p>The renovations to the Color Compass Calgary office space and training centre are now complete. As indicated in an earlier Connected issue the renovations started on November 18th with the project under development for 6 months. What started with increasing the space for one office quickly morphed into the idea of adding even more offices, a larger lunchroom space, increased classroom size for our training centre, and a larger boardroom. The first step was renewing a 10 year lease agreement on Oct. 1st, work then commenced with an architect, general contractor and interior designer to work with the current square footage and achieve the targeted improvements, and by the looks of the new space that target was hit.</p>
+	<p>The reno's include;</p>
+	<ul>
+		<li>A training centre classroom with double the capacity (From 20 to 40 people), with state of the art projection, sound and presentation ability. This will allow us to hold larger groups for I-CAR, BASF and Akzo Nobel driven education workshops and certification classes as well as various Industry events.</li>
+		<li>3 new offices which help to harmonize Chase and Carlson work environments by moving them to same side of the building.</li>
+		<li>3 new purpose built forward facing order desks ground and a larger retail showroom space to promote a more inviting walk-in clientele.</li>
+		<li>A much larger lunchroom with new cabinetry/appliances accommodates more seating and amenities.</li>
+		<li>The entire space received new flooring, paint, millwork, washrooms etc. as well upgraded furnishings to many areas.</li>
+	</ul>
+	<p>Jamie Corbeil, Southern Alberta Sales Manager for Carlson and Project Lead for the renovations, couldn't hide his excitement of how everything turned out "We are really happy with the results, what we now have is a larger more inviting space for both our customers and staff. The ability to now hold larger training clinics and industry events will really set Color Compass far above the competition, if you're ever in the Calgary area please stop by!" </p>
+	<div class="row small-up-1 medium-up-4">
+		<div class="column padded-vertical"><img src="/images/news/img-3260.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3261.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3262.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3263.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3264.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3265.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3266.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3270.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3271.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3272.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3274.jpg" alt="Exciting Changes"></div>
+	</div>
+	<div class="row small-up-1 medium-up-3">
+		<div class="column padded-vertical"><img src="/images/news/img-3267.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3268.jpg" alt="Exciting Changes"></div>
+		<div class="column padded-vertical"><img src="/images/news/img-3273.jpg" alt="Exciting Changes"></div>
+	</div>
+</div>
