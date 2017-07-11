765431efb8df681f7791952d11af3d1ae17097a2
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-11 17:18:54 +0800

    add

---
+++ views/news/exciting_changes_happening_calgary.php
@@ -0,0 +1,20 @@
+<div class="news">
+	<h5>Exciting Changes Happening in Calgary</h5>
+	<p>There is a lot of excitement around new renovations happening to the Calgary office space and training centre. The renovations started on November 18th with the project under development for the past 6 months. What started with increasing the space for one office quickly morphed into the idea of adding even more offices, a larger lunchroom space, increased classroom size for our training centre, and a larger boardroom. First step was renewing a 10 year lease agreement on Oct. 1st,  work then commenced with an architect, general contractor and interior designer to work with the current square footage and achieve the targeted improvements.</p>
+	<p>The reno's will include;</p>
+	<ul>
+		<li>A training centre classroom with double the capacity (From 20 to 40 people), with state of the art projection, sound and presentation ability. This will allow us to hold larger groups for I-CAR , BASF and Akzo Nobel driven education workshops and certification classes as well as various Industry events.</li>
+		<li>3 new offices which help to harmonize Chase and Carlson work environments by moving them to same side of the building.</li>
+		<li>3 new purpose built forward facing order desks ground and a larger retail showroom space to promote a more inviting walk-in clientele.</li>
+		<li>A much larger lunchroom with new cabinetry/appliances accommodates more seating and amenities.</li>
+		<li>The entire space is getting new flooring, paint, millwork, washrooms etc. as well upgraded furnishings to many areas.</li>
+	</ul>
+	<p>We had the chance to quickly chat with Jamie Corbeil, Southern Alberta Sales Manager with Carlson, "It's a very exciting time in Calgary. These new upgrades will carry us into the next decade and enable us to be at the fore front in providing a world class facility and experience for our customers and staff."</p>
+	<p>We look forward to highlighting the "After Images" in future Connected issues. </p>
+	<div class="row small-up-1 medium-up-4">
+		<div class="column padded-vertical"><img src="/images/news/exciting-changes1.jpg" alt="Exciting Changes 1"></div>
+		<div class="column padded-vertical"><img src="/images/news/exciting-changes2.jpg" alt="Exciting Changes 2"></div>
+		<div class="column padded-vertical"><img src="/images/news/exciting-changes3.jpg" alt="Exciting Changes 3"></div>
+		<div class="column padded-vertical"><img src="/images/news/exciting-changes4.jpg" alt="Exciting Changes 4"></div>
+	</div>
+</div>
