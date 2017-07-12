9900eaaffae9efdf2dc0b3c3ae5c1fd53c41acc6
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-12 16:15:53 +0800

    add grind

---
+++ views/news/grind_to_paradise2_winner.php
@@ -0,0 +1,8 @@
+<div class="news">
+	<h5>Grind to Paradise 2 Winner</h5>
+	<div class="image padded">
+		<img src="/images/news/grind-paradise2-winner.jpg" alt="Grind To Paradise 2 Winner">
+	</div>
+	<p>By simply purchasing some select 3M Cubitron II abrasives one White &amp; Peters customer's spring became a little sunnier! Frank Lamb of Kootenay Autobody, pictured with Dwain Black of White & Peters Kelowna, couldn't be happier with his "beat the odds" win. With purchases that earned him two ballots Frank surely did beat the odds, some customers had earned and qualified for over 30 entries through the Color Compass 3M Grind to Paradise 2 promotion. The Grind to Paradise 2 promotion is the second of its kind and rewarded customers for purchasing select 3M products exclusively through Color Compass. For each $200 purchased during a two month period the customer received an entry ballot for a grand prize $3,500 travel voucher good for any travel of the winners choice.</p>
+	<p>And Frank's choice? We hear a Cuba trip has already been in the plans, and with the win those plans can become a little more real for Frank and his wife. We couldn't be happier to present the winning prize to Frank and thank him for his loyalty with White & Peters and reward him for his passion of working on cars and managing his team at Kootenay Autobody.</p>
+</div>
