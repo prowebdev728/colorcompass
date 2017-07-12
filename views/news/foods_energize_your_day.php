96e1e7ac6b8a994e9771f73b66e355cc1e5ef2c1
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-12 16:15:37 +0800

    add

---
+++ views/news/foods_energize_your_day.php
@@ -0,0 +1,97 @@
+<div class="news">
+	<h5>Foods To Energize Your Day</h5>
+	<div class="image">
+		<img src="/images/news/foods-energize-your-day.jpg" alt="Foods To Energize Your Day">
+	</div>
+	<p>Now we're not saying you should abstain from coffee completely; that would be just crazy! Instead keep this list handy when selecting your next meal or when you start to feel sluggish — use these tips and you'll be feeling more energetic and recharged in no time!</p>
+	<ol>
+		<li>Water:
+			<p>Water, water, water – you hear it every day, but for good reason! Our bodies are about 60% water so yes, this means your skin, muscles, and brain is mostly made of water. Studies suggest that even mild dehydration can slow your metabolism and reduce your energy. Keep hydrated with water and avoid "hunger for thirst." Remember, you should drink at least eight glasses of water per day. A few tips to help up your water intake:</p>
+			<ul>
+				<li>Add lemon or orange</li>
+				<li>Add basil, mint and cucumber</li>
+				<li>A pinch of cayenne pepper</li>
+				<li>Switch up flat for bubbly water</li>
+			</ul>
+		</li>
+		<li>Nuts, Seeds, and Oils:
+			<p>Fear the jar of peanut butter or bag of almonds? Just because these crunchy treats have a higher fat content doesn't necessarily mean we shouldn't be eating them. We should be! They are healthy fats and your bod loves them. Just remember, use portion control and you'll feel satisfied for longer so you're not grazing all day long. Just 1 ounce of almonds (23 almonds) is packed with protein (6 g) and contains minerals (magnesium, calcium, phosphorus, potassium). Trust us, fat is your friend! You need fat nutrients to stay satisfied and to burn excess fat. If you aren't eating nuts and nut butters at snack or mealtimes, use a little coconut or avocado oil when cooking. Ways to up your healthy fat content:</p>
+			<ul>
+				<li>1 handful of almonds on the go</li>
+				<li>2 teaspoons of peanut butter on apple slices</li>
+				<li>Mash 1/4 avocado on Ezekiel bread toast</li>
+				<li>Sprinkle chia, hemp, or flax seeds on your morning yogurt or toss into a smoothie</li>
+				<li>Stir fry with coconut oil</li>
+			</ul>
+		</li>
+		<li>Greek Yogurt:
+			<p>The Mediterranean is famous for both healthy and delicious eats, but Greek culture certainly wins when it comes to knowing what to do with yogurt. Greek yogurt is higher in protein and lower in sugar and sodium than traditional yogurt. Greek yogurt will keep you fuller longer and that makes your belly happy. Stick to 2% plain or whole fat and add your own drizzle of honey or splash of vanilla or cacao. This is an easy, on-the-go breakfast option if you are running out the door - and it will keep you going until lunch! Remember Greek yogurt can be used:</p>
+			<ul>
+				<li>In quick breads, cakes, muffins, pancakes, sweets, and baked goods</li>
+				<li>As a great meat marinade or dip base with veggies</li>
+				<li>For a quick and easy dessert - dip blueberries into Greek yogurt and freeze</li>
+			</ul>
+		</li>
+		<li>Bananas:
+			<p>Bananas contain essential vitamins that provide natural energy and the best part? They don't need to be dressed up! A plain banana with a slab of nut butter is an awesome snack as bananas provide a lasting energy boost without the common crash. One medium banana has 105 calories and is loaded with electrolyte potassium (422 grams) so keep them close. Ways to incorporate bananas in your meals:</p>
+			<ul>
+				<li>Make a sweet farro or quinoa salad with sliced bananas</li>
+				<li>Spread nut butter for on top for a protein packed snack</li>
+				<li>Freeze them for a refreshing snack</li>
+				<li>Use them while baking and cut out some of the flour</li>
+			</ul>
+		</li>
+		<li>Lean Protein:
+			<p>Protein is such a talked-up nutrient in the media that many of us are always worried about getting enough. In reality, most of us have too much protein in our diets! There is at least a little protein in many things we eat and we really don't need that much for our bodies to function properly. But, when choosing what types of lean protein to eat — choice wisely! Protein is needed for many bodily functions including cell maintenance and repair, blood clotting, and production of antibodies. Lean protein is also important for energy, and provides satiety so we don’t overeat between meals. Good protein choices:</p>
+			<ul>
+				<li>Baked salmon with lemon and fresh herbs with salt and pepper</li>
+				<li>Grill a grass-fed beef burger, nix the bun, and have a sweet potato instead</li>
+				<li>Add a hard-boiled egg to the side of your A.M. green juice</li>
+				<li>Grab a handful of walnuts at the same time you grab an apple</li>
+			</ul>
+		</li>
+		<li>DHA Fortified Eggs:
+			<p>Nowadays there is no such thing as one type of egg. There are brown, white, grass-fed, organic…the options seem endless! Eggs make for are an awesome breakfast, lunch, or dinner. They are high in choline, which supports nervous system function and lessens inflammation. All eggs naturally contain small amounts of DHA, but DHA enriched eggs (all-natural eggs from hens fed a special DHA rich diet, usually flaxseed) can contain up to 150mg of DHA per egg. The incredible, edible egg:</p>
+			<ul>
+				<li>Bake in half an avocado</li>
+				<li>Poach on top of your salad (ditch the dressing)</li>
+				<li>Scramble and toss into a grain salad like you would with fried rice</li>
+			</ul>
+		</li>
+		<li>Lentils:
+			<p>These little beans are nutritional gems. It's time to think beyond lentil soup and get creative because lentils are here to stay! Research shows that lentils contain unique proteins that can lower levels of cholesterol and triglycerides in the bloodstream. They also contain plenty of fiber which keeps you feeling full, and they have impressive amounts of B vitamins, iron, and zinc. Remember:</p>
+			<ul>
+				<li>Lentils are great in salads, soups, stews</li>
+				<li>Use them as a meat alternative in dishes like tacos</li>
+			</ul>
+		</li>
+		<li>Dark Leafy Greens:
+			<p>Spinach, kale, and their other green siblings seem to be all the rage, and for good reason. Not only high in vitamins A and C, these leafy greens are loaded with B vitamins, aka "the energy vitamins." Remember, there are many fun ways to eat your greens beyond your favorite salad! Try these:</p>
+			<ul>
+				<li>Throw a handful into your next smoothie</li>
+				<li>Top pizza or pasta</li>
+				<li>Toss into your soup</li>
+			</ul>
+		</li>
+		<li>Whole Grains:
+			<p>Carbohydrates are our body's main source of fuel; they provide the necessary energy to literally keep us up and running! However, not all carbs are created equal. Always choose whole grain products (not to be confused with multigrain or whole wheat). Although brown rice, oats, and whole wheat are the usual staples, it's time broaden your grainy horizons. Next grocery shop, replace a traditional grain with an ancient one (farro, quinoa, spelt, kamut). Use these grains in your breakfast to kick start your day! Try these morning meals:</p>
+			<ul>
+				<li>Buckwheat pancakes</li>
+				<li>Farro salad with basil and tomato-mozz</li>
+				<li>Millet porridge</li>
+				<li>Spelt blueberry muffins</li>
+			</ul>
+		</li>
+		<li>Dark Chocolate:
+			<p>Always crave a little something sweet after lunch or dinner? Right there with you! Dark chocolate contains a natural stimulant theobromine (similar to our caffeinated friend, coffee) that gives you a feel good buzz. Turns out, chocolate may also give your love life a little boost - this treat is commonly thought of as an aphrodisiac. Aim for 1 ounce (or one square) of 70% cacao or more. Try this:</p>
+			<ul>
+				<li>1 ounce dark chocolate straight up with a cup of herbal tea</li>
+				<li>Melt ½ ounce dark chocolate and dip or drizzle over strawberries or dried apricots</li>
+				<li>Add a few tablespoons of cocoa powder to coffee, yogurt, or oatmeal</li>
+			</ul>
+		</li>
+	</ol>
+	<p>Lastly, and don’t forget...<br>
+		When it comes to energy, it takes two to tango. Beyond eating the right foods, it is important to not forget a daily dose of sweat. We all know exercise burns calories, but it also helps us sleep better, boosts our mood, and speeds up our metabolism. Many times we leave the gym sweaty, sore, and dare I say, energized? That's right, when your heart is pumping your body releases endorphins that increase energy levels. And as always, be sure to stay hydrated before, during, and after any sweat session. Eat right, feel the burn, and up the ante on your energy levels!
+	</p>
+</div>
