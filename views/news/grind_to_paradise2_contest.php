82648ecfa7a32203c6c46efd28ef540c9c1c15cd
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-12 16:15:48 +0800

    add grind

---
+++ views/news/grind_to_paradise2_contest.php
@@ -0,0 +1,36 @@
+<div class="news">
+	<h5>Color Compass 3M Grind To Paradise 2 Contest</h5>
+	<div class="image padded">
+		<img src="/images/news/grind-paradise2.jpg" alt="Grind To Paradise 2">
+	</div>
+	<p>Starting November 1st 2016, our Jobber divisions consisting of Carlson, Chase, White & Peters, and Rocky Mountain participated in a 3M sponsored internal sales contest and market-wide promotion. The Grind to Paradise 2 promotion gave our customers a shot at a $3500 travel voucher for every $200 purchased of Cubitron II and Accuspray products.  For the internal sales contest we really tried to drive growth on the participating products and had prizes for the top sales reps within each division that achieved the highest sales growth.</p>
+	<div class="row small-up-1 medium-up-3">
+		<div class="column">
+			The Chase winners were:
+			<ol>
+				<li>Dan Tougas -  Winnipeg</li>
+				<li>Danny Felske - Winnipeg</li>
+				<li>Garry Hopp -  Langley</li>
+				<li>Jay Patel -  Vancouver</li>
+			</ol>
+		</div>
+		<div class="column">
+			The White &amp; Peters winners were:
+			<ol>
+				<li>Lawrence Bradley - Langley</li>
+				<li>John Johnston - Kelowna</li>
+				<li>Peter Lui - Vancouver</li>
+				<li>Dwain Black - Kelowna</li>
+			</ol>
+		</div>
+		<div class="column">
+			The Carlson and Rocky Mountain winners were:
+			<ol>
+				<li>Adam Pacholko - Regina</li>
+				<li>Jason Hall - Calgary</li>
+				<li>Brent Campbell - Edmonton</li>
+			</ol>
+		</div>
+	</div>
+	<p>Congratulations to the winners and the sales teams on a successful promotion!<br>The winning customer draw will take place on Wednesday February 8th. </p>
+</div>
