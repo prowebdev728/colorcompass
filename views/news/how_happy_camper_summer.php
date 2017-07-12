e47d143acfb7dab1f3e48aaa29138ec61890f60c
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-12 16:16:06 +0800

    add how_happy

---
+++ views/news/how_happy_camper_summer.php
@@ -0,0 +1,31 @@
+<div class="news">
+	<h5>How to Be A Happy Camper This Summer</h5>
+	<p>As people across the country flee cities for the relaxation of a camp site over summer weekends, experts are offering urbanites some reminders on how to stay safe while communing with nature. No matter where in Canada you're pitching a tent, they say a little planning goes a long way.</p>
+	<p><strong>Unpredictable weather</strong></p>
+	<p>Inclement weather can crop up quickly and unexpectedly. Geoff Coulson, a warning preparedness meteorologist with Environment Canada, says disaster can be avoided if an emergency plan is in place.</p>
+	<p>He says it's a good idea to get oriented in a campsite when you arrive, much the same way you would in a hotel room.</p>
+	<p>"It's important to scope out the safety plan during the calm of a sunny day when there is no threatening weather, so if it hits at 2 a.m., you're prepared," he says.</p>
+	<p>In thunder and lightning storms the safest spot to be is in a car with an all-metal roof. If that's not an option, he advises going further into the woods, as opposed to your tent, which has metal poles that pose a danger. Just make sure you don't settle under the tallest tree.</p>
+	<p>"Make every effort to get out of your tent," he says.</p>
+	<p>If you're in a flat area, he says it's a good idea to sit cross-legged in a low-lying area, but not a ditch, which will collect more water, allowing the lightning to travel more.</p>
+	<p>Many city folk are "divorced from nature," he points out, so they often forget how merciless mother nature can be. He says one of the first things you pack for camping should always be a flashlight with extra batteries, just in case.</p>
+	<p>Jeri Syroteuk, a spokeswoman with Fundy National Park in New Brunswick, located right along the Bay of Fundy, advises campers to pack layers of clothing. Along the water, it's often five to 10 degrees colder than it is inland.</p>
+	<p>"That's really important because the weather changes quickly in this area, so bring a sweater and rain jacket," she says.</p>
+	<p><strong>Don't go it alone</strong></p>
+	<p>Ontario Parks suggest making sure someone is aware of where you are at all times. That way, if you get lost, they'll have a better chance of tracking you down.</p>
+	<p>It's a good idea to bring a cellphone too, especially since phones aren't just phones anymore, and can often double as a GPS. Ontario Parks also suggests bringing a whistle and having your campsite name and number on hand.</p>
+	<p>Mosher says he always makes a point of having an exit plan in case someone gets sick or breaks a limb. He tries to plan routes near more accessible roads, and advises bringing a GPS in case there's no cell reception, as is often the case in the wilderness.</p>
+	<p>"Being prepared is the major concern here," he says.</p>
+	<p><strong>Ticks</strong></p>
+	<p>According to The Public Health Agency of Canada, there are known tick populations in parts of Ontario, B.C., Manitoba, New Brunswick, Nova Scotia and Quebec.</p>
+	<p>The best way to avoid playing host to a tick is to wear light clothing that covers the skin, footwear and socks that cover the feet, use repellent with DEET, and pay close attention to the groin, armpit and scalp areas at the end of the day to ensure no ticks are present.</p>
+	<p><strong>Camp fires</strong></p>
+	<p>The Ministry of Natural Resources posts tips on its website on how to build safe camp fires. The most important points to remember are to build them on bare patches of dirt and to keep the fire about 15 meters away from buildings or tents.</p>
+	<p><strong>Be sun-savvy</strong></p>
+	<p>Camping and hiking trips often call for extended periods of time in the sun. Syroteuk cautions that it's always important to bring sunscreen and reapply often, even when travelling to national parks, which are often shady.</p>
+	<p>"You can burn even on a rainy or cloudy day," she says.</p>
+	<p>Though it's obvious, she says, it's still important to remember to stay hydrated.</p>
+	<p><strong>If all else fails</strong></p>
+	<p>There's an app for that. Parks Canada has launched a "learn to camp" app that's available for free on the app store and on the agency's website. <a href="https://www.pc.gc.ca/en/multimedia/apps">https://www.pc.gc.ca/en/multimedia/apps</a></p>
+	<p>And to get you started we’re giving away a $100 Canadian Tire Gift Card! Simply respond to the Connected Newsletter with your favourite summer camping spot to be entered into the random draw.</p>
+</div>
