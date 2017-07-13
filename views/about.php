5c04d5040310eb7a2665fab210b68441829aac14
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-13 16:28:15 +0800

    add about page

--- views/about.php
+++ views/about.php
@@ -1,14 +1,14 @@
-<div class="row intro bpadding">
+<div class="row padded-bottom">
 	<img src="/images/about-us-banner.jpg" alt="About Color Compass Corporation">
 </div>
-<div class="row bpadding">
-	<div class="small-12 column">
+<div class="row">
+	<div class="small-12 column padded-vertical">
 		<p>Color Compass Corporation, founded in 1984, is a  private automotive distribution group, currently specializing in the supply of collision repair products and industry specific collision repair training throughout Canada. Based in Western Canada, the companies that comprise the Color Compass Corporation; Carlson Body Shop Supply, Chase Auto Body Supplies, Rocky Mountain Sales, White & Peters, and PBE Distributors  each have a long and rich history in their respective markets. Collectively our group delivers products, training, and solutions that enhance our customer’s production and profitability. Currently Color Compass is a team of over 260 specialists within 27 Canadian locations who operate four distinct Western Canadian jobber operations, as well as a national warehousing division; <strong>we have a national reach with a local touch.</strong></p>
 		<p>At Color Compass we understand that all of our customers are different. We take a consultative approach to build tailored services, solutions, and product offerings that meet our clients specific business objectives. Partnering with a Color Compass company will allow you the peace of mind to focus on your customers and growing your business.</p>
 	</div>
 </div>
 <div class="row">
-	<div class="small-12 column">
+	<div class="small-12 column text-center">
 		<img src="/images/about-map.jpg" alt="Compass Corporation Locations">
 
 	</div>
