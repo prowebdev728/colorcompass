c52c8b029c9b3830f5d0320a186561ff30f31d73
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-18 05:03:57 +0800

    add news page

--- views/news.php
+++ views/news.php
@@ -1,11 +1,11 @@
-		<div class="row intro bpadding">
+		<div class="row padded-bottom">
 			<img src="/images/news-banner.jpg">
 		</div>
 		<div class="row">
-			<div class="small-12 medium-12 column">
+			<div class="small-12 medium-12 column news">
 				<h3>Color Compass News</h3>
 				<?=$new_news;?>
-				<h4>Color Compass Past News</h4>
+				<h3>Color Compass Past News</h3>
 				<?=$old_news;?>
 			</div>
 		</div>
