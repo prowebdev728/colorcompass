7d6a9e8b31f1e89916b9be34c515f5b728fe2f2f
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-18 05:03:51 +0800

    add mission page

--- views/mission.php
+++ views/mission.php
@@ -1,23 +1,22 @@
-		<div class="row intro bpadding">
-			<img src="/images/mission-banner.jpg">
-		</div>
-		<div class="row">
-			<div class="small-12 column">
-				<h3>Our Mission</h3>
-				<p><em>&quot;Color Compass leads the automotive refinish and specialty market distribution industries by providing sustainable and profitable business solutions based on innovative products and tailor made services.
-					We foster continuous improvement by creating a culture of caring for our people, our suppliers, and our customers.&quot;</em></p>
-			</div>
-			<div class="small-12 column">
-				<h3>Our Vision</h3>
-				<p><strong>Sustained growth, defined by:</strong></p>
-				<dl class="vision">
-					<dt>People:</dt><dd>Be a great place to work where people are inspired to be the best they can be. Create and sustain a culture of effective communications.</dd>
-					<dt>Products:</dt><dd>Be the supplier of premium quality, excellent value refinishing products that allow owners and technicians to achieve high levels of customer satisfaction.</dd>
-					<dt>Partners:</dt><dd>Nurture a winning, balanced network of new and loyal customers. Together with our solutions-focused suppliers, we create mutual, enduring value based on collaboration and trust.</dd>
-					<dt>Planet:</dt><dd>Be a responsible corporate citizen that strives to make a difference by choosing products, services and operational practices that are sustainable and environmentally respectful.</dd>
-					<dt>Profit:</dt><dd>Maximize long-term return to the corporation and solid financial rewards to employees while satisfying customer needs.</dd>
-					<dt>Productivity:</dt><dd>Be a highly effective and fast-moving organization. Be an example for customers seeking improved operational & technical efficiency and effectiveness.</dd>
-				</dl>
-
-			</div>
-		</div>
+<div class="row padded-bottom">
+	<img src="/images/mission-banner.jpg">
+</div>
+<div class="row padded-vertical">
+	<div class="small-12 column">
+		<h3>Our Mission</h3>
+		<p><em>&quot;Color Compass leads the automotive refinish and specialty market distribution industries by providing sustainable and profitable business solutions based on innovative products and tailor made services.
+			We foster continuous improvement by creating a culture of caring for our people, our suppliers, and our customers.&quot;</em></p>
+	</div>
+	<div class="small-12 column">
+		<h3>Our Vision</h3>
+		<p><strong>Sustained growth, defined by:</strong></p>
+		<dl class="vision">
+			<dt>People:</dt><dd>Be a great place to work where people are inspired to be the best they can be. Create and sustain a culture of effective communications.</dd>
+			<dt>Products:</dt><dd>Be the supplier of premium quality, excellent value refinishing products that allow owners and technicians to achieve high levels of customer satisfaction.</dd>
+			<dt>Partners:</dt><dd>Nurture a winning, balanced network of new and loyal customers. Together with our solutions-focused suppliers, we create mutual, enduring value based on collaboration and trust.</dd>
+			<dt>Planet:</dt><dd>Be a responsible corporate citizen that strives to make a difference by choosing products, services and operational practices that are sustainable and environmentally respectful.</dd>
+			<dt>Profit:</dt><dd>Maximize long-term return to the corporation and solid financial rewards to employees while satisfying customer needs.</dd>
+			<dt>Productivity:</dt><dd>Be a highly effective and fast-moving organization. Be an example for customers seeking improved operational & technical efficiency and effectiveness.</dd>
+		</dl>
+	</div>
+</div>
