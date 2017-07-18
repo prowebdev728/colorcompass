35f6f1e0ec22f659236413a943fe89318391c9e5
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-19 01:47:23 +0800

    update university page, pixel perfect, mobile/tablet/desktop responsive

---
+++ views/university.php
@@ -0,0 +1,306 @@
+<style type="text/css">
+	.ccc-university .leading {
+		position: relative;
+		margin-top: 2rem;
+	}
+	.ccc-university .leading img {
+		width: 100%;
+		height: 100%;
+	}
+	.ccc-university .leading > div {
+		position: absolute;
+		top: 0;
+		left: 0;
+		width: 100%;
+	}
+	.ccc-university .leading > div div:nth-of-type(1) {
+		padding: 5.625rem 3.375rem 5.625rem 3.375rem;
+	}
+	.ccc-university .leading h3 {
+		font-family: "Ramabhadra";
+		color: #fff;
+		font-size: 5.625rem;
+		float: left;
+		line-height: 1;
+		text-shadow: 0.188rem 0.25rem 0.188rem #666;
+	}
+	.ccc-university .leading h3:nth-of-type(2) {
+		float: right;
+	}
+	.ccc-university .leading h3.mobile-leading-title {
+		display: none;
+		text-align: center;
+	}
+	.ccc-university .leading > div div:nth-of-type(2) {
+		float: left;
+		margin-top: 3.75rem;
+		background-color: rgba(255, 255, 255, 0.68);
+		padding: 3.5rem 3.125rem 2.5rem 3.125rem;
+	}
+	.ccc-university .leading > div div:nth-of-type(2) p {
+		font-family: "Open Sans Condensed";
+		font-weight: 300;
+		font-size: 2.438rem;
+		line-height: 1.3;
+	}
+
+	.ccc-university .training {
+		padding: 2.5rem 0 1.5rem 3.5rem !important;
+	}
+	.ccc-university .training p {
+		font-family: "Open Sans Condensed";
+		font-weight: 300;
+		font-size: 2.063rem;
+		line-height: 1.3;
+	}
+
+	.ccc-university .browse {
+		padding: 1.5rem 7.5rem !important;
+	}
+	.ccc-university .browse > div {
+		position: relative;
+		margin-bottom: 1.25rem;
+	}
+	.ccc-university .browse img {
+		width: 32.063rem;
+	}
+	.ccc-university .browse p {
+		width: 24.5rem;
+		height: 8rem;
+	}
+	.ccc-university .browse img + p {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		margin-top: -4rem;
+		margin-left: -12.25rem;
+		padding: 2rem 0 !important;
+
+		background-color: rgba(0, 0, 0, 0.5);
+		font-size: 3.5rem;
+		font-weight: bold;
+	}
+	.ccc-university .browse > div:nth-of-type(2) img + p {
+		padding: 2.375rem 0 !important;
+		font-size: 3rem;
+	}
+	.ccc-university .browse > div:nth-of-type(4) img + p {
+		padding: 3rem 0 !important;
+		font-size: 2rem;
+	}
+
+	@media screen and (max-width: 90rem) {
+		.ccc-university .leading h3 {
+			font-size: 4.5rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(2) p {
+			font-size: 2.25rem;
+		}
+
+		.ccc-university .training p {
+			font-size: 1.625rem;
+		}
+
+		.ccc-university .browse {
+			padding: 1.5rem 3rem !important;
+		}
+	}
+
+	@media screen and (max-width: 75rem) {
+		.ccc-university .leading {
+			margin-top: 2rem;
+		}
+		.ccc-university .leading h3 {
+			font-size: 3.75rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(1) {
+			padding: 3.75rem 2.813rem 3.75rem 3rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(2) {
+			margin-top: 2.5rem;
+			padding: 2.25rem 3rem 1.25rem 3rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(2) p {
+			font-size: 1.875rem;
+		}
+
+		.ccc-university .training {
+			padding: 2rem 0 0.75rem 0 !important;
+		}
+		.ccc-university .training p {
+			font-size: 1.438rem;
+		}
+
+		.ccc-university .browse {
+			padding: 0 3rem 1.5rem 3rem !important;
+		}
+	}
+
+	@media screen and (max-width: 64rem) {
+		.ccc-university .leading h3 {
+			font-size: 2.25rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(1) {
+			padding: 3.625rem 2.25rem 1.5rem 2.25rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(2) {
+			margin-top: 1.875rem;
+			padding: 1.125rem 1.5rem 0.375rem 1.5rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(2) p {
+			font-size: 1.375rem;
+		}
+
+		.ccc-university .training {
+			padding: 2rem 0 0rem 0 !important;
+		}
+		.ccc-university .training p {
+			font-size: 1.125rem;
+		}
+
+		.ccc-university h1 {
+			font-size: 2.625rem !important;
+		}
+
+		.ccc-university .browse {
+			padding: 0 1.5rem 1.5rem 1.5rem !important;
+		}
+		.ccc-university .browse img {
+			width: 28.125rem;
+		}
+	}
+
+	@media screen and (max-width: 40rem) {
+		.ccc-university .leading {
+			margin-top: 0.75rem;
+		}
+		.ccc-university .leading img {
+			height: 28.375rem;
+		}
+		.ccc-university .leading h3 {
+			font-size: 2rem;
+			display: none;
+		}
+		.ccc-university .leading h3.mobile-leading-title {
+			display: block !important;
+		}
+		.ccc-university .leading > div div:nth-of-type(1) {
+			padding: 1.75rem 0.75rem 1.75rem 0.75rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(2) {
+			margin-top: 1.125rem;
+			padding: 1.125rem 1.5rem 0.375rem 1.5rem;
+		}
+		.ccc-university .leading > div div:nth-of-type(2) p {
+			font-size: 1.25rem;
+			line-height: 1.2;
+		}
+
+		.ccc-university .training {
+			padding: 1.125rem 0 0rem 0 !important;
+		}
+		.ccc-university .training div:nth-of-type(2) {
+			padding: 1.125rem 1.125rem 0 1.125rem;
+		}
+		.ccc-university .training p {
+			font-size: 1.25rem;
+			line-height: 1.2;
+		}
+
+		.ccc-university h1 {
+			font-size: 2.25rem !important;
+		}
+
+		.ccc-university .browse {
+			padding: 0 0 1.5rem 0 !important;
+		}
+		.ccc-university .browse img {
+			width: 18.75rem;
+		}
+		.ccc-university .browse p {
+			width: 16.25rem;
+			height: 6.25rem;
+		}
+		.ccc-university .browse img + p {
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			margin-top: -3.125rem;
+			margin-left: -8.125rem;
+			padding: 1.75rem 0 !important;
+
+			background-color: rgba(0, 0, 0, 0.5);
+			font-size: 2.625rem;
+			font-weight: bold;
+		}
+		.ccc-university .browse > div:nth-of-type(2) img + p {
+			padding: 2.125rem 0 !important;
+			font-size: 2rem;
+		}
+		.ccc-university .browse > div:nth-of-type(4) img + p {
+			padding: 2.5rem 0 !important;
+			font-size: 1.375rem;
+		}
+	}
+
+	@media screen and (max-width: 23.125rem) {
+		.ccc-university .leading img {
+			height: 33.375rem;
+		}
+	}
+</style>
+
+<div class="row padded-bottom">
+	<img src="/images/university-banner.jpg" alt="excellence">
+</div>
+<div class="ccc-university">
+	<div class="row leading">
+		<img src="/images/training-gears-full.jpg" alt="training courses">
+		<div>
+			<div>
+				<h3>Leading Edge Training For</h3>
+				<h3>Today's Collision Repair Shop</h3>
+				<h3 class="mobile-leading-title">Leading Edge Training For Today's Collision Repair Shop</h3>
+			</div>
+			<div>
+				<p>Welcome to Color Compass University, where our goal is to facilitate and provide the very best in training to the Collision Repair industry. Color Compass University offers both scheduled and on-demand technical training and business management workshops. From comprehensive welding training utilizing the latest in equipment, to industry leading production, business management, and financial performance courses; Color Compass University will help your business thrive.</p>
+			</div>
+		</div>
+	</div>
+	<div class="row training">
+		<div class="small-12 medium-6 columns"><img src="/images/graph.jpg" alt="graph"></div>
+		<div class="small-12 medium-6 columns">
+			<p>There are four Color Compass University training centres throughout western Canada, with locations in Edmonton, Calgary, Winnipeg, and Greater Vancouver. Each location is filled with the latest in cutting-edge repair and refinish equipment, as well as interactive classrooms and presentation areas that can facilitate workshops and meetings as large as 30 people.</p>
+			<p>As the industry evolves so do our programs and course content, we have partnered with the top manufactures, training organizations, and collision business training companies in the industry to ensure that the latest education is always available.</p>
+		</div>
+	</div>
+	<div class="row column text-center">
+		<h1>Browse Our Programs</h1>
+	</div>
+	<div class="row small-up-1 medium-up-1 large-up-2 browse">
+		<div class="column text-center">
+			<a href="/university/paint" class="">
+				<img src="/images/paint-refinish.jpg" alt="paint-refinish">
+				<p class="button large radius">Paint + Refinsh</p>
+			</a>
+		</div>
+		<div class="column text-center">
+			<a href="/university/business">
+				<img src="/images/business-management.jpg" alt="business-management">
+				<p class="button large radius">Business Management</p>
+			</a>
+		</div>
+		<div class="column text-center">
+			<a href="/university/welding">
+				<img src="/images/welding-repair.jpg" alt="welding-repair">
+				<p class="button large radius">Welding + Repair</p>
+			</a>
+		</div>
+		<div class="column text-center">
+			<a href="/university/automotive">
+				<img src="/images/automotive-management.png" alt="automotive-management">
+				<p class="button large radius">Automotive Management Institute</p>
+			</a>
+		</div>
+	</div>
+</div>
