b8fbf7aa2b2419ed3e71b18df2fbdb540a2bcd3c
prowebdev728 <proweb.developer2016@yandex.com>
2017-07-19 16:09:56 +0800

    add

--- views/news/details.php
+++ views/news/details.php
@@ -1,8 +1,8 @@
-<div class="row intro bpadding">
+<div class="row padded-bottom">
 	<img src="/images/news-banner.jpg">
 </div>
 <div class="row">
-	<div class="small-12 medium-12 column">
+	<div class="small-12 medium-12 column news">
 		<?=$content;?>
 	</div>
 </div>
