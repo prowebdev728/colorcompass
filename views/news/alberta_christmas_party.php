8e0113b9063c57d2ef628377a8821c383010b8b4
proweb dev <proweb.developer2016@yandex.com>
2017-08-15 11:44:30 +0800

    views news alberta

--- views/news/alberta_christmas_party.php
+++ views/news/alberta_christmas_party.php
@@ -1,6 +1,6 @@
 <div class="news">
 	<div class="image text-center">
-		<img src="/images/news/fairmont-hotel.jpg" alt="Fairmont Hotel">
+		<img src="/images/fairmont-hotel.jpg" alt="Fairmont Hotel">
 	</div>
 	<h5>Alberta Christmas Party</h5>
 	<p>Color Compass kicked off the holiday season on Saturday, November 26th  with our first of two annual holiday parties at the beautiful Fairmont Hotel MacDonald in Downtown Edmonton. The evening was a great success with a turnout of 200 guests, our largest party to date! Among the attendees; a few of our generous manufacturers and sponsors; BASF, Akzo, Caruk &amp; Associates, Saint Gobain and 3M among others – We thank you for your contributions.</p>
@@ -24,17 +24,17 @@
 	<p class="padded-top text-center">Special note to Kelly Skedanuk of Carlson Edmonton for 30 years of service!<br>and<br>Kevin Tomiak of Carlson Edmonton for 40 years of service!</p>
 	<p>We would like to take this time to thank Chris Kikuchi, Rob Neale and Gord Milford for allowing us to host another lovely holiday event this year, and we look forward to seeing the BC staff and guests at our holiday party in Vancouver this Saturday, December 10th!</p>
 	<div class="row small-up-2 medium-up-4 padded-top">
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas1.jpg" alt="Alberta Christmas Party 1"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas2.jpg" alt="Alberta Christmas Party 2"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas3.jpg" alt="Alberta Christmas Party 3"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas4.jpg" alt="Alberta Christmas Party 4"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas5.jpg" alt="Alberta Christmas Party 5"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas6.jpg" alt="Alberta Christmas Party 6"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas7.jpg" alt="Alberta Christmas Party 7"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas8.jpg" alt="Alberta Christmas Party 8"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas9.jpg" alt="Alberta Christmas Party 9"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas10.jpg" alt="Alberta Christmas Party 10"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas11.jpg" alt="Alberta Christmas Party 11"></div>
-		<div class="column padded-vertical"><img src="/images/news/alberta-xmas12.jpg" alt="Alberta Christmas Party 12"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas1.jpg" alt="Alberta Christmas Party 1"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas2.jpg" alt="Alberta Christmas Party 2"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas3.jpg" alt="Alberta Christmas Party 3"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas4.jpg" alt="Alberta Christmas Party 4"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas5.jpg" alt="Alberta Christmas Party 5"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas6.jpg" alt="Alberta Christmas Party 6"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas7.jpg" alt="Alberta Christmas Party 7"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas8.jpg" alt="Alberta Christmas Party 8"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas9.jpg" alt="Alberta Christmas Party 9"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas10.jpg" alt="Alberta Christmas Party 10"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas11.jpg" alt="Alberta Christmas Party 11"></div>
+		<div class="column padded-vertical"><img src="/images/alberta-xmas12.jpg" alt="Alberta Christmas Party 12"></div>
 	</div>
 </div>
