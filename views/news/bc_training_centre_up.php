ac5df032325c2ec73b4e81a1de9feb6bdd7912d4
proweb dev <proweb.developer2016@yandex.com>
2017-08-21 01:05:56 +0800

    add news pages

--- views/news/bc_training_centre_up.php
+++ views/news/bc_training_centre_up.php
@@ -1,7 +1,7 @@
 <div class="news">
 	<h5>Color Compass Training Centre in BC Up &amp; Running</h5>
 	<div class="image">
-		<img src="/images/news/bc-training-centre-up.jpg" alt="BC Training Centre">
+		<img src="/images/bc-training-centre-up.jpg" alt="BC Training Centre">
 	</div>
 	<p>The new BC Color Compass training centre is now complete with training already underway at the Coquitlam office. As a group Color Compass now has four training centres in Western Canada, far beyond what any other distributor can provide the industry.</p>
 	<p>The newly minted facility features a 3,400 square foot technical area complete with a Global Ultra advanced down draft spray booth, the Nitro Heat spray system, hip blowers, 15000 CFM AMU , and an 8 x8 prep deck for test panels and smaller training scenarios. On the way is a Car-O-Liner Quick 42 mobile rack, Pro Spot i5 resistance welder, Pro Spot SP- 5 Multi welder, Wedge Clamp Eclipse measuring system, and the Wedge Clamp Nitro Plastic Nitrogen Welder. The centre is also equipped with the full line of BETAG Innovation products.</p>
@@ -9,29 +9,29 @@
 	<p>To open the training centre we welcomed BETAG Innovation as well as over 40 staff, estimators, and facility managers in for four days of segmented in-class and technical training. The training session was in conjunction with the BMW approval of the BETAG equipment for small-damage repair. The four days comprised of an in-class estimator course, as well as technical small-damage repair training where attendees were able to get their hands on the full BETAG system.</p>
 	<p>Betag Innovation is based in Switzerland and has been working with Color Compass to bring their repair solutions to Canada. Much like Color Compass, BETAG realizes success lies in a well-trained industry. According to BETAG, while the company does manufacture the equipment they sell, the focus is always on training techs to perform higher quality and more efficient repairs using that equipment. "We focus on upskilling," they say. "Training is a really big part of what we do. We want to teach technicians to perform higher quality repairs as well as producing the tools that allow them to do that." The Betag small damage repair and medium panel repair programs that are facilitated at Color Compass are specifically designed for BMW, and the programs are all part of the current BMW North America Group University curriculum.</p>
 	<div class="row small-up-2 medium-up-3 padded-top">
-		<div class="column padded-vertical"><img src="/images/news/bc-training-001.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-002.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-003.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-004.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1519.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1520.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1521.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1522.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1523.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1525A.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1527.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1528.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1529.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1530.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1531.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1532.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1533.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1534.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1535.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1536.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1537.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1538.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1539.jpg" alt="BC Training Center"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-training-1540.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-001.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-002.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-003.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-004.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1519.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1520.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1521.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1522.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1523.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1525A.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1527.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1528.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1529.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1530.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1531.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1532.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1533.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1534.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1535.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1536.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1537.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1538.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1539.jpg" alt="BC Training Center"></div>
+		<div class="column padded-vertical"><img src="/images/bc-training-1540.jpg" alt="BC Training Center"></div>
 	</div>
 </div>
