0a346e11609779a6c7e2ac3201d131ccd1dc3161
proweb dev <proweb.developer2016@yandex.com>
2017-08-21 01:06:10 +0800

    add news pages

--- views/news/calgary_facility_gets_kubed.php
+++ views/news/calgary_facility_gets_kubed.php
@@ -1,16 +1,16 @@
 <div class="news">
 	<h5>Calgary Facility Gets KUBE'd</h5>
 	<div class="image">
-		<img src="/images/news/kube-logo.png" alt="Kube Logo">
+		<img src="/images/kube-logo.png" alt="Kube Logo">
 	</div>
 	<p>One of the largest collision repair facilities in Calgary, and the largest Toyota Collision Centre in Western Canada, Country Hills Toyota recently jumped into the Color Compass KUBE. In a month where Country Hills was experiencing one of their busiest sales months to date, the Color Compass KUBE team went to work. With a lot of the pre-work done; including the inventory audit, installation of inventory cabinets, cleaning and organizing the production environment, the CCC team then spent the better part of a week working on finding the right balance of inventory (min/max), tracking specific consumption, barcode labelling inventory cabinets, and technician training on the scanner.</p>
 	<p>After ten days, 150 inventory scans by 18 technicians and staff members the KUBE team confidently reported...no issues! The Country Hills inventory was lowered by 15% with no rush orders required. The total Carlson deliveries in those ten days were down from the usual 3 to 2 (-33%), which may not seem like a lot but can make a big difference for our operations and theirs.</p>
 	<p>Emile Fremont, the Director of Business Development, was encouraged that such a large facility experienced such positive results, "Country Hills Toyota is the largest single facility to date that we have installed KUBE IM, what is really amazing is that all of their techs are scanning without hesitation, which is part of our KUBE vision". Emile also pointed to their team environment which led to immediate buy-in, "At Country Hills they have a great team environment in the shop, each technician is curious and wants to help push their shop to the next level, we couldn't be happier with the results". </p>
 	<div class="row small-up-1 medium-up-3">
-		<div class="column padded-vertical"><img src="/images/news/calgary-kube1.jpg" alt="Calgary Kubed 1"></div>
-		<div class="column padded-vertical"><img src="/images/news/calgary-kube2.jpg" alt="Calgary Kubed 2"></div>
-		<div class="column padded-vertical"><img src="/images/news/calgary-kube3.jpg" alt="Calgary Kubed 3"></div>
-		<div class="column padded-vertical"><img src="/images/news/calgary-kube4.jpg" alt="Calgary Kubed 4"></div>
-		<div class="column padded-vertical"><img src="/images/news/calgary-kube5.jpg" alt="Calgary Kubed 5"></div>
+		<div class="column padded-vertical"><img src="/images/calgary-kube1.jpg" alt="Calgary Kubed 1"></div>
+		<div class="column padded-vertical"><img src="/images/calgary-kube2.jpg" alt="Calgary Kubed 2"></div>
+		<div class="column padded-vertical"><img src="/images/calgary-kube3.jpg" alt="Calgary Kubed 3"></div>
+		<div class="column padded-vertical"><img src="/images/calgary-kube4.jpg" alt="Calgary Kubed 4"></div>
+		<div class="column padded-vertical"><img src="/images/calgary-kube5.jpg" alt="Calgary Kubed 5"></div>
 	</div>
 </div>
