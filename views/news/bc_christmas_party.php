c477541b6141cacfc84e9cc03ae21429f569d186
proweb dev <proweb.developer2016@yandex.com>
2017-08-21 01:05:38 +0800

    add news pages

--- views/news/bc_christmas_party.php
+++ views/news/bc_christmas_party.php
@@ -1,7 +1,7 @@
 <div class="news">
 	<h5>Color Compass BC Christmas Party</h5>
 	<div class="image">
-		<img src="/images/news/bc-xmas.jpg" alt="Color Compass BC Christmas Party">
+		<img src="/images/bc-xmas.jpg" alt="Color Compass BC Christmas Party">
 	</div>
 	<p>The Color Compass BC staff wrapped up 2016 with a fabulous annual holiday party on Saturday, December 10th at the Marriott Pinnacle Downtown Vancouver. As in Alberta, the Vancouver party had another record breaking 200 guests in attendance. Among those guests were our wonderful manufacturers and business partners; 3M, BASF, Akzo Nobel, Caruk &amp; Associates, DW Fontaine, RBC, and Miller Thomson. We thank them for their continued support and contributions.</p>
 	<p>Not only did we enjoy a delicious plated dinner followed by dance and a (hilarious at times) holiday photo booth, we were also introduced to an evening of wonders by illusionist Vitaly Beckham. Vitaly managed to astonish even the most doubting audience member with his mind-bending magical acts. We still haven't figured out how he managed to make all of those drinks disappear!</p>
@@ -25,22 +25,22 @@
 	<p class="padded-top text-center">Special note to Dave Casselman of W&amp;P for 30 years of service.</p>
 	<p>Once again, we would like to take this time to thank Chris Kikuchi, Rob Neale and Gord Milford for allowing us to host another lovely holiday event and we wish everyone a happy, and prosperous new year!</p>
 	<div class="row small-up-2 medium-up-4 padded-top">
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas4.jpg" alt="BC Christmas Party 4"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas5.jpg" alt="BC Christmas Party 5"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas6.jpg" alt="BC Christmas Party 6"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas7.jpg" alt="BC Christmas Party 7"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas8.jpg" alt="BC Christmas Party 8"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas9.jpg" alt="BC Christmas Party 9"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas10.jpg" alt="BC Christmas Party 10"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas11.jpg" alt="BC Christmas Party 11"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas12.jpg" alt="BC Christmas Party 12"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas4.jpg" alt="BC Christmas Party 4"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas5.jpg" alt="BC Christmas Party 5"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas6.jpg" alt="BC Christmas Party 6"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas7.jpg" alt="BC Christmas Party 7"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas8.jpg" alt="BC Christmas Party 8"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas9.jpg" alt="BC Christmas Party 9"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas10.jpg" alt="BC Christmas Party 10"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas11.jpg" alt="BC Christmas Party 11"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas12.jpg" alt="BC Christmas Party 12"></div>
 	</div>
 	<div class="row small-up-2 medium-up-3 padded-top">
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas1.jpg" alt="BC Christmas Party 1"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas2.jpg" alt="BC Christmas Party 2"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas3.jpg" alt="BC Christmas Party 3"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas14.jpg" alt="BC Christmas Party 14"></div>
-		<div class="column padded-vertical"><img src="/images/news/bc-xmas15.jpg" alt="BC Christmas Party 15"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas1.jpg" alt="BC Christmas Party 1"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas2.jpg" alt="BC Christmas Party 2"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas3.jpg" alt="BC Christmas Party 3"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas14.jpg" alt="BC Christmas Party 14"></div>
+		<div class="column padded-vertical"><img src="/images/bc-xmas15.jpg" alt="BC Christmas Party 15"></div>
 	</div>
 
 
