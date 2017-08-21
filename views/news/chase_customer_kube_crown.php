fbe48de25ab35d45e61df5d55c7216a99b479aca
proweb dev <proweb.developer2016@yandex.com>
2017-08-21 23:48:05 +0800

    add news pages

--- views/news/chase_customer_kube_crown.php
+++ views/news/chase_customer_kube_crown.php
@@ -1,11 +1,11 @@
 <div class="news">
 	<h5>Chase Customer is a KUBE Crown</h5>
 	<div class="image">
-	<img src="/images/news/chase-kube-crown.jpg" alt="Chase Customer is a KUBE Crown">
+	<img src="/images/chase-kube-crown.jpg" alt="Chase Customer is a KUBE Crown">
 	</div>
 	<p>Crown Autobody & Glass located in Winnipeg launched KUBE IM just three short months ago and in that time it has become as much a part of their daily occurrence as the morning production meeting. With 27 technicians scanning regularly, the now  present routine of scanning and replenishing their materials has resulted in an average of an eight thousand dollar order every Monday  and Thursday,  with consistent but ever turning inventory; a big win for any business utilizing inventory on a regular basis.</p>
 	<p>What's more impressive for Crown and the local Chase branch is that in the last 2 months, they have not phoned in a single order using the custom order function in KUBE for materials that are needed over and above the inventory set levels. The custom order function allows an order to be pushed through in special circumstances, like a greater demand for specific products due to a spike in production, or more demand for one toner in a given week, etc.</p>
 	<p>Moving forward Crown is now looking to KUBE DS in giving them a platform to manage all of their SOP and internal documentation. With KUBE DS Crown can easily bring up all of their technical data sheets, repair specific SOPs, and even store a lot of their HR documentation.</p>
 	<p>Special shout out to Sean Ashford and Chedric Lubi, as well as all of the Chase Manitoba staff for their vigilance and constant monitoring.  Also, special thank you to the staff and management at Crown for embracing KUBE as a team and helping it become a success.</p>
-		<img src="/images/news/kube-cabinet.jpg" alt="Chase Customer is a KUBE Crown"><br><small>Just one of the Crown KUBE IM cabinets</small>
+		<img src="/images/kube-cabinet.jpg" alt="Chase Customer is a KUBE Crown"><br><small>Just one of the Crown KUBE IM cabinets</small>
 </div>
