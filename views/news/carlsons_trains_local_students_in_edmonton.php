ea7d384e8b1cc0452af72a7020dfee19d030afb6
proweb dev <proweb.developer2016@yandex.com>
2017-08-21 23:46:54 +0800

    new page

---
+++ views/news/carlsons_trains_local_students_in_edmonton.php
@@ -0,0 +1,22 @@
+<div class="news">
+	<h4><strong>Carlson&#39;s Trains Local Students in Edmonton</strong></h4>
+
+<div class="image text-center"><img alt="" src="/images/news/Carlson-Trains-Local-Students.jpg" /></div>
+
+<p>At the Color Compass tech centre in Edmonton local area students from Centre High are often seen getting an introduction to collision repair. The program all started in 2006 when recently retired Cliff Reihl, former Technical Rep with Carlson, approached Centre High with a vision to introduce their students to the career possibilities of automotive refinish. Included with that would be professional training by our volunteer staff as well as full use of the Color Compass training facility and classroom.&nbsp;&nbsp;</p>
+
+<p>The program has slowly evolved into what is expected as starting positions within a repair facility;</p>
+
+<p>Day One - Basics of Prep</p>
+
+<p>Day Two - Detailing and Polishing</p>
+
+<p>Day Three - Paint Protection Film Application</p>
+
+<p>Bill Allen, Technical Rep with Carlson in Edmonton, explained a little about the program &quot;We do a three day class. In the past it was three days of basic prep.&nbsp;Now I have evolved it into a couple different things that would be starting positions in a body shop. This year we did one day of basic prep; cleaning, sanding, feather-edging, then priming body panels. Once primer has dried then we block sand and prepare for paint. So it is just the basic procedures; these students in most cases have never had these tools in their hand.&quot; &nbsp;&quot;Second day is detail polishing, we had painted hoods with dirt nibs and other imperfections in it. We show them the steps to polish to perfection, and the third and final day is Paint Protection Film, using the Durashield product.&quot;</p>
+
+<p>&nbsp;</p>
+
+<p>Centre High is located Downtown Edmonton and provides a blended-learning campus offering secondary and post-secondary studies all under one roof. Students attend Centre High Campus to finish their high school studies, upgrade existing marks, enroll in specific courses for their career, or to start post-secondary studies.</p>
+
+</div>
