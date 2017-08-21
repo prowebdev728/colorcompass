1a9da46cb7cb19caef8afbe1d392b7572c025037
proweb dev <proweb.developer2016@yandex.com>
2017-08-21 23:47:25 +0800

    add news pages

--- views/news/carlson_chase_new_tech.php
+++ views/news/carlson_chase_new_tech.php
@@ -1,7 +1,7 @@
 <div class="news">
 	<h5>Carlson &amp; Chase Highlight New Technology</h5>
 	<div class="image text-center">
-		<img src="/images/news/carlson-chase2.jpg" alt="Carlson &amp; Chase Highlight New Technology">
+		<img src="/images/carlson-chase2.jpg" alt="Carlson &amp; Chase Highlight New Technology">
 	</div>
 	<p>Carlson and Chase put Color Compass on the map in Saskatchewan recently with two New Product Showcase events in Saskatoon and Regina. The events took place Tuesday March 21st and Thursday March 23rd  at Saskatchewan Polytechnic’s Saskatoon and Regina campuses.</p>
 	<p>There was a concentration on live demonstrations, giving repairers in Saskatchewan the opportunity to get their hands on some of the latest products and services available all under one roof. The events also served as a good chance to network directly with manufacturers' representatives. The enthusiasm from the industry was clear with some attendees driving over three hours each way to attend. Door prizes were also up for grabs at both events with the winners in Saskatoon being; Sandberg Collision, North Battleford Campbell's Auto Body, Wydena, and Magic Paint &amp; Body, Saskatoon.</p>
@@ -11,16 +11,16 @@
 	<p>Approximately 75 industry stakeholders attended the event in Saskatoon, with an additional 100 or so repairers attending in Regina. </p>
 	<p>The New Products Showcases featured the latest on 3-D measuring, digital marketing programs, nitrogen welding, small dent repair and more. Exhibiting manufacturers included Pro-Spot, Car-O-Liner, BETAG, Wedge Clamp, 3M, Pure Auto Digital Marketing, Kent Automotive, Prestige Chemical, DuraShield, CPS, Farecla, Booth Brothers and many more.</p>
 	<div class="row small-up-1 medium-up-4">
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase3.jpg" alt="New Technology 3"></div>
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase4.jpg" alt="New Technology 4"></div>
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase5.jpg" alt="New Technology 5"></div>
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase7.jpg" alt="New Technology 7"></div>
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase8.jpg" alt="New Technology 8"></div>
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase14.jpg" alt="New Technology 14"></div>
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase1.jpg" alt="New Technology 1"></div>
-		<div class="column padded-vertical"><img src="/images/news/carlson-chase6.jpg" alt="New Technology 6"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase3.jpg" alt="New Technology 3"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase4.jpg" alt="New Technology 4"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase5.jpg" alt="New Technology 5"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase7.jpg" alt="New Technology 7"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase8.jpg" alt="New Technology 8"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase14.jpg" alt="New Technology 14"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase1.jpg" alt="New Technology 1"></div>
+		<div class="column padded-vertical"><img src="/images/carlson-chase6.jpg" alt="New Technology 6"></div>
 	</div>
 	<div class="row column text-center padded-top">
-		<img src="/images/news/carlson-chase9.jpg" alt="New Technology 9">
+		<img src="/images/carlson-chase9.jpg" alt="New Technology 9">
 	</div>
 </div>
