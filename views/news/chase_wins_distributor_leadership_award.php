0ed37fabc54fad652b21a3f8900512d1b8a295fa
proweb dev <proweb.developer2016@yandex.com>
2017-08-23 11:09:42 +0800

    add news page

--- views/news/chase_wins_distributor_leadership_award.php
+++ views/news/chase_wins_distributor_leadership_award.php
@@ -3,7 +3,7 @@
 	<p>Sarasota, Florida – October 18th, 2016 – At the annual AkzoNobel Distributor Conference in Sarasota Florida Chase Auto Body Supplies was presented with the prestigious AkzoNobel Distributor Leadership Award.</p>
 	<p>This award is given to the distributor within the entire Canadian Distribution network that contributes in communication, and working with AkzoNobel through cooperative alignment to provide more overall value for their customers.  The award is presented to the distributor that drives innovation, leverages tools and resources in providing excellent customer service, and has a keen focus of reinvesting back into their business. They are active in industry charities and focused on sustainable (long-term) sales growth through collaborative market planning in the markets they serve.</p>
 	<div class="image">
-		<img src="/images/news/chase-leadership-award.jpg" alt="Chase Leadership Award">
+		<img src="/images/chase-leadership-award.jpg" alt="Chase Leadership Award">
 	</div>
 	<p>Why Chase was selected</p>
 	<ul>
