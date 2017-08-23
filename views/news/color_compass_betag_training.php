b931e4f795364082ed2ed718123a5d673d14d303
proweb dev <proweb.developer2016@yandex.com>
2017-08-23 11:10:01 +0800

    add news page

--- views/news/color_compass_betag_training.php
+++ views/news/color_compass_betag_training.php
@@ -1,30 +1,30 @@
 <div class="news">
 	<h5>Color Compass Hosts BETAG BMW Training</h5>
 	<div class="image">
-		<img src="/images/news/betag-training1.jpg" alt="Color Compass Hosts BETAG BMW Training">
+		<img src="/images/betag-training1.jpg" alt="Color Compass Hosts BETAG BMW Training">
 	</div>
 	<p>Recently Color Compass welcomed BETAG Innovation to its Edmonton Training Centre for three days of technical and in-class training. The training was in conjunction with the BMW approval of the BETAG equipment for small-damage repair. The three days comprised of an in-class estimator course, as well as technical small-damage repair training where attendees were able to use the BETAG system. Attendance was quite strong, with a cross section of the collision repair industry including estimators, insurance personnel, repair technicians, and management.</p>
 	<p>Betag Innovation is based in Switzerland and have been working with Color Compass to bring Betag's repair solutions to Canada. Repair solutions is the right term, according to BETAG, while the company does manufacture the equipment they sell, the focus is always on training techs to perform higher quality and more efficient repairs using that equipment. "We focus on upskilling,"  they say. "Training is a really big part of what we do. We want to teach technicians to perform higher quality repairs as well as producing the tools that allow them to do that." The Betag small damage repair and medium panel repair programs that were facilitated at Color Compass are specifically designed for BMW, and the programs are all part of the current BMW North America Group University curriculum.</p>
 	<div class="row small-up-1 medium-up-3">
 		<div class="column column-block">
-			<img src="/images/news/betag-training2.jpg" class="thumbnail" alt="BETAG BMW Training">
+			<img src="/images/betag-training2.jpg" class="thumbnail" alt="BETAG BMW Training">
 		</div>
 		<div class="column column-block">
-			<img src="/images/news/betag-training4.jpg" class="thumbnail" alt="BETAG BMW Training">
+			<img src="/images/betag-training4.jpg" class="thumbnail" alt="BETAG BMW Training">
 		</div>
 		<div class="column column-block">
-			<img src="/images/news/betag-training5.jpg" class="thumbnail" alt="BETAG BMW Training">
+			<img src="/images/betag-training5.jpg" class="thumbnail" alt="BETAG BMW Training">
 		</div>
 	</div>
 	<div class="row small-up-1 medium-up-3">
 		<div class="column column-block">
-			<img src="/images/news/betag-training3.jpg" class="thumbnail" alt="BETAG BMW Training">
+			<img src="/images/betag-training3.jpg" class="thumbnail" alt="BETAG BMW Training">
 		</div>
 		<div class="column column-block">
-			<img src="/images/news/betag-training6.jpg" class="thumbnail" alt="BETAG BMW Training">
+			<img src="/images/betag-training6.jpg" class="thumbnail" alt="BETAG BMW Training">
 		</div>
 		<div class="column column-block">
-			<img src="/images/news/betag-training7.jpg" class="thumbnail" alt="BETAG BMW Training">
+			<img src="/images/betag-training7.jpg" class="thumbnail" alt="BETAG BMW Training">
 		</div>
 	</div>
 </div>
