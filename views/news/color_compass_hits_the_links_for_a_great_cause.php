f245257b0004b858fb722304b6ce95db43c2c4e6
proweb dev <proweb.developer2016@yandex.com>
2017-08-23 11:10:17 +0800

    add news page

---
+++ views/news/color_compass_hits_the_links_for_a_great_cause.php
@@ -0,0 +1,32 @@
+<div class="news"><h4>Color Compass Hits the Links for a Great Cause</h4>
+
+<div class="image text-center"><img alt="" src="/images/news/image1.jpg" /></div>
+
+<p>Color Compass was proud to be a sponsor at the recent CSN Collision Centres 11th annual golf tournament held at Piper&rsquo;s Heath Golf Club in Milton, Ontario.</p>
+
+<p>On top of the sponsorship, four Color Compass staff braved the stormy weather to post a very respectable 6 under, which was just outside the winning numbers.</p>
+
+<p>For years the CSN tournament has always been about more than just the golf. The company&rsquo;s goal is to build a stronger internal community through this tournament, but it also helps to improve larger communities as well.</p>
+
+<p>&ldquo;This tournament is our way of being good citizens and giving back to our community,&rdquo; said Trokic. &ldquo;We have a duty to help those who cannot help themselves. By organizing this tournament, we give ourselves hope that we are contributing in bringing happiness into the lives of children suffering from sicknesses beyond our understanding. This in itself is why this tournament is such a success, and why we continue to host it year after year.&rdquo;</p>
+
+<p>And &ldquo;giving back&rdquo; is certainly what they did. $10,000 dollars was presented to Megan McGill of Make-A-Wish Canada, $6,530 of which was raised by participating guests. Megan&#39;s wish was to see the Hollywood sign up close and personal as well as take in all that LA has to offer, with a stop in Disneyland no less.</p>
+
+<div class="medium-up-4 padded-top row small-up-2">
+<div class="column padded-vertical"><img alt="" src="/images/news/image2.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/image3.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/image4.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/image5.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/image6.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/image7.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/image8.jpg" /></div>
+</div>
+
+
+</div>
