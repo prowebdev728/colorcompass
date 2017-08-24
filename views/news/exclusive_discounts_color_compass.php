02dfc2dde8e0c53036798270ac9a0ac4defe795f
proweb dev <proweb.developer2016@yandex.com>
2017-08-24 21:38:51 +0800

    add news page

--- views/news/exclusive_discounts_color_compass.php
+++ views/news/exclusive_discounts_color_compass.php
@@ -1,6 +1,6 @@
 <div class="news">
 	<h5>Exclusive discounts for Color Compass staff on Dell Electronics</h5>
 	<div class="text-center">
-	<img src="/images/news/dell-color-compass.jpg" alt="Dell Color Compass">
+	<img src="/images/dell-color-compass.jpg" alt="Dell Color Compass">
 	</div>
 </div>
