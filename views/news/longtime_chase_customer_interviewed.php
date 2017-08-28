fcf2f193a4cf144227b8c80c79eb85b536517e74
prowebdev728 <proweb.developer2016@yandex.com>
2017-08-28 23:23:35 +0800

    add news page

---
+++ views/news/longtime_chase_customer_interviewed.php
@@ -0,0 +1,17 @@
+<div class="news">
+	<h5>Longtime Chase Customer Interviewed By Akzo Nobel</h5>
+	<p>Akzo Nobel recently visited Craig Dunn GM in Portage la Prairie Manitoba to discuss with their Fixed Operations Manager, Joel McPhail, the impact that Akzo's new Automatchic Vision (AMV) system is having. Craig Dunn Collision Centre has been a loyal Chase Auto Body Supplies customer since 1992, and has always been a market leader in innovation, and finding the best equipment for their facility. To that end they were early adopters with the new (AMV) system, making it an integral part of their refinish process. The team at Akzo Nobel took notice and thought that Joel and his team were perfect candidates for their AMV shop video.</p>
+	<p>Joel had this to say during the video about the new technology and the impact that is has in their facility, "The camera is one of the greatest tools we have implemented into our repair process in a decade. We rely on it, and trust it. It removes the element of human error out of the equation when selecting the right (colour) variant. We have a 100% success rate with the camera. We will never go back to not using the camera, this technology is just too superior."</p>
+	<div class="image">
+		<img src="/images/hand-held-device.jpg" alt="Longtime Chase Customer Interviewed By Akzo Nobel">
+	</div>
+	<p>The Automatchic Vision is the latest generation of Akzo Nobel’s hand-held device that digitally analyzes color; and Automatchic Smart Search – color retrieval software that provides the optimum matching color formula. The compact Automatchic Vision device uses the latest digital technology to guarantee precise, reliable measurements of color, including curved parts of a vehicle that are traditionally difficult to read.</p>
+	<p>The system is also intuitive and easy to operate, so that only minimal training is needed to achieve accurate measurements quickly. Color readings obtained using Automatchic Vision easily translated into the best matching color formula by the Smart Search software.</p>
+	<p>Colors are getting more complicated because of the use of special effects such as metallics and pearlescents. Automatchic Vision ensures the optimum color match whilst minimizing paint use, reducing waste and saving valuable time.</p>
+	<p>Thank you to Danny Felske at Chase Winnipeg for the heads up!</p>
+	<div class="row small-up-2 medium-up-3 padded-top">
+		<div class="column padded-vertical"><img src="/images/chase-interview1.jpg" alt="Chase Customer Interviewed"></div>
+		<div class="column padded-vertical"><img src="/images/chase-interview2.jpg" alt="Chase Customer Interviewed"></div>
+		<div class="column padded-vertical"><img src="/images/chase-interview3.jpg" alt="Chase Customer Interviewed"></div>
+	</div>
+</div>
