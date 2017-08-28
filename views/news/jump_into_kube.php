ab2c1d580f2430812b59a14b373a150aa2987b55
prowebdev728 <proweb.developer2016@yandex.com>
2017-08-28 23:22:57 +0800

    add news page

---
+++ views/news/jump_into_kube.php
@@ -0,0 +1,5 @@
+<div class="news">
+	<h5>Jump Into the KUBE!</h5>
+	<img src="/images/jump-into-kube.jpg" alt="Jump Into the KUBE" style="width: 90%;">
+	<p>Stay Tuned For More Color Compass KUBE IMDS News!</p>
+</div>
