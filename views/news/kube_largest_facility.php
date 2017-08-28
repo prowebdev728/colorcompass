be4d2c837d80a66f60c9873f8c3b0cf87cbac576
prowebdev728 <proweb.developer2016@yandex.com>
2017-08-28 23:23:18 +0800

    add news page

---
+++ views/news/kube_largest_facility.php
@@ -0,0 +1,19 @@
+<div class="news">
+	<h5>KUBE IM Launches at Single Largest Facility to Date</h5>
+	<div class="image">
+		<img src="/images/kube-largest.jpg" alt="KUBE IM Launches at Single Largest Facility to Date">
+	</div>
+	<p>Crown Autobody and Glass, a loyal Chase Auto Body Supplies customer for over 20 years and one of the premier shops in the Winnipeg market, has successfully set up our KUBE Inventory Management system. Based in Winnipeg, Manitoba, with a 12,000 square foot space and 26 techs, Crown is the largest single facility to implement KUBE IM to date!</p>
+	<p>Leading the Crown KUBE IM deployment were Chase’s KUBE Customer Service Rep, Chedrick Lubi and CCC's Business Development Specialist, Sean Ashford.</p>
+	<h5>The Deployment Process</h5>
+	<p>Following their initial meeting with Crown Autobody and Glass, Chedrick and Sean completed this project in a total of three weeks. The first two weeks were spent introducing KUBE IM, discussing program limitations, determining min/max inventory levels, consolidating materials and setting up KUBE/Navision. Upon finalizing logistics, Chedrick and Sean were able to deploy and go live with KUBE IM within 7 days – quite the turnaround.</p>
+	<p>Deployment consisted of installing three cabinets: One for paint and two for materials. Further consolidation to remove minimal and duplicate inventory took place throughout the course of deployment with the support of the shop techs.</p>
+	<p>To complete successful deployment, Chedrick and Sean spent two days training with the shop techs and familiarizing them with the KUBE IM scanner. Currently, 24 of the 26 shop techs at Crown Autobody and Glass are utilizing the scanners with two KUBE stars on the shop floor assisting their fellow techs.</p>
+	<p>To say the KUBE IM deployment at Crown Autobody and Glass was a success would be an understatement. Chedrick and Sean were able to remove almost $7,000 in inventory. Based on $70,000 purchases, that is a 10% reduction! To date, the shop has done over 600 scans. Next on the horizon for Crown – KUBE DS, documented solutions to map out all their shop processes.</p>
+	<p>"The success of this project is in great part due to the work of Chedrick Lubi. This was Chedrick’s first install and he is becoming a champion for KUBE. He recently moved from inside sales to split duties of KUBE Manitoba Team Lead and has still maintained inside sales duties. He has really risen to the task and deserves recognition." Sean Ashford, Color Compass Business Development Specialist</p>
+	<div class="row small-up-2 medium-up-3 padded-top">
+		<div class="column padded-vertical"><img src="/images/kube-launches2.jpg" alt="Kube Launches"></div>
+		<div class="column padded-vertical"><img src="/images/kube-launches3.jpg" alt="Kube Launches"></div>
+		<div class="column padded-vertical"><img src="/images/kube-launches4.jpg" alt="Kube Launches"></div>
+	</div>
+</div>
