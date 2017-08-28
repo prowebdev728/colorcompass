af9ec3211885d04c20842661d4596a9357787a13
prowebdev728 <proweb.developer2016@yandex.com>
2017-08-28 23:23:49 +0800

    add news page

---
+++ views/news/looking_toward_2017.php
@@ -0,0 +1,40 @@
+<div class="news">
+	<div class="row">
+		<div class="small-12 medium-6 column">
+			<h5>Looking Toward 2017</h5>
+			<p>As we look toward an exciting year for the collision repair industry and Color Compass, we recently sat down with three Color Compass General Managers, Mort Hall of White & Peters, Tim Shay of Chase, and Dave Swenson of Carlson to get their perspective on 2017.</p>
+		</div>
+		<div class="small-12 medium-6 column text-center">
+			<img src="/images/looking-toward-2017.jpg" alt="Looking Toward 2017">
+		</div>
+	</div>
+	<hr>
+	<div class="row column">
+		<div class="image text-center">
+			<img src="/images/hs-mort.jpg" alt="Mort Hall" class="smll"><br>
+			<small>Mort Hall, General Manager, White &amp; Peters</small>
+		</div>
+		<p>Exciting times ahead for White &amp; Peters, the BC economy should continue to be the leading economy amongst the provinces for the next 3-4 years, which means more people moving here to live and work and more cars on the road, which means more accidents and more paint sales.  Our customer base is made up mostly of the most innovative and well equipped shops in the province and as more and more car manufacturers and insurance companies will require shops to have advanced training and equipment before they will be approved to fix New Car Technology, our customer base will have the upper-hand in these approval processes and will reap some long deserved rewards for their training and equipment initiatives that they have been working on for the last few years. We as a company will continue to assist our customers in meeting and exceeding the repair guidelines that will need to be achieved, with initiatives like KUBE and our soon to be launched in-house training centre. Long story short our customers base is made up of winners and our business should grow as they do over the next few years and we should continue to see productive and busy times ahead.</p>
+	</div>
+	<hr>
+	<div class="row column">
+		<div class="image text-center">
+			<img src="/images/hs-tim.jpg" alt="Tim Shay" class="smll"><br>
+			<small>Tim Shay, General Manager, Chase Auto Body Supplies</small>
+		</div>
+		<p>As we enter the New Year of 2017 we reflect back on 2016 and we realize how fortunate we are to be working for such a great company. The Automotive Repair Industry is in the crux of major change through manufacturers expectations, Insurers expectations and the consolidation of repair facilities-not to mention the consumer expectations. We find ourselves aligned with the best customers in the western provinces that will enable us to attain the growth goals that we have set for 2017. The equipment requirements that are being mandated will enable us to provide our customers with the necessary equipment and training due to the people that we have working for us and the product lines that we carry. We will still need to maintain a strong presence on the service and support side of our business as our customers are becoming more sophisticated and require a higher level of "touch" in today’s ever changing market. I look forward to a great 2017 and working together to make it the best that it can be!</p>
+	</div>
+	<hr>
+	<div class="row column">
+		<div class="image text-center">
+			<img src="/images/hs-dave.jpg" alt="Dave Swenson" class="smll"><br>
+			<small>Dave Swenson, General Manager, Carlson Body Shop Supply,<br> Rocky Mountain Sales </small>
+		</div>
+		<p>I am very excited for 2017, not that I think things will be easier this year, or that the Alberta economy will have a magical turnaround taking us back to prior days when hitting the forecasts was a given. This year will still be mostly hard work and servicing our customer base to the best of our ability; making sure above all that we do not lose any accounts.</p>
+		<p>Our business is always in a state of change, what I am excited about is how fast new vehicle technology is accelerating the changes. New technology such as, lane departure warnings, forward looking radar, automatic braking, pedestrian detection, heads up display in the windshield, parking assist, vehicle to vehicle communication, self-driving cars, the list goes on and on. New materials like Aluminum, Boron, Carbon fiber, light weight steels like Nano steel, soon to be Magnesium, Titanium, and more. New processes like Mig brazing, Riveting, Tig welding, Carbon fiber repair, Vehicle scanning technology, automated inventory management in real time (KUBE IM)New programs like MSO accreditation, continuation and advancements of lean technologies.</p>
+		<p>We at Color Compass are very well situated to help our customers navigate through all these changes that are occurring. These are the reasons that we have invested in the many training centers that we have, partnered with groups like I-CAR, developed KUBE IMDS, started to develop Color Compass University, why we have aligned ourselves with the many different groups and consolidators in the marketplace, finally and why we have developed and will continue to develop the best people in the industry. Our customers and customers in waiting are looking to us and you to help them understand all of these industry changes, and provide the training they require to be able to properly and safely repair this new technology.</p>
+		<p>So again I am very excited to be part of all this change, our job as a company will be to first, educate ourselves to understand these changes, then get this information out to our customers and customers in waiting. Get them into our training centers to be trained, and then help them to implement these changes so that they stay at the top of the collision repair business.</p>
+		<p>Strong loyal customers moving forward ensure our long term success.</p>
+		<p>So my message for 2017 is, learn all you can, communicate what you have learned to your customer base, help them to succeed and make sure that you stay relevant both within your customer base and within your own company.</p>
+	</div>
+</div>
