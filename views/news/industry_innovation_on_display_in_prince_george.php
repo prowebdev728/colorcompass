f5def574e80c68e4f92ed1a8e9cafe4d9af27968
prowebdev728 <proweb.developer2016@yandex.com>
2017-08-28 23:22:46 +0800

    add news page

---
+++ views/news/industry_innovation_on_display_in_prince_george.php
@@ -0,0 +1,36 @@
+<div class="news">
+	<h4><strong>Industry Innovation on Display in Prince George </strong></h4>
+
+<p>White &amp; Peters and Chase Auto Body Supplies recently came together to hold an Industry Innovation Show at the College of New Caledonia. The goal of the event was to provide the market of Prince George and Northern BC an opportunity to explore how the industry is rapidly evolving.</p>
+
+<p>Many companies were involved to showcase their equipment and products. The show was highlighted with the latest in advanced repair equipment from Prospot, Wedge Clamp, BETAG, CPS, Dent Fix, and B-Tec. Aside from the latest in repair equipment, attendees were also able to see the latest products from vendors like Norton, Carbo, Fusor, Devilbiss, 3M and SATA. Held at the College of New Caledonia&#39;s technical school, attendees were given a chance to see the new equipment and products live and in action. Also on display was the digital automotive marketing service Pure Auto, as well as the new inventory management and digital document solution KUBE from Color Compass.</p>
+
+<p>&quot;We couldn&#39;t be happier with the turnout that we had at the show, we really wanted to bring all that is new and upcoming in the industry to the Northern BC market in an interactive and hands-on way,&rdquo; said Sean Skoropat, Sales Manager at White &amp; Peters. &quot;We at Color Compass look forward to bringing more of these informative events to each market we serve as we move forward.&quot; Sean also thanks the College of New Caledonia for providing its amazing technical facility and the staff and vendor partners that participated and helped make the show a success.</p>
+
+<div class="medium-up-4 padded-top row small-up-2">
+<div class="column padded-vertical"><img alt="" src="/images/news/PG8.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG10%20(2).jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG10%20(1).jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG11.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG12.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG2%20(2).jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG2%20(1).jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG3.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG4.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG5.jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG7%20(2).jpg" /></div>
+
+<div class="column padded-vertical"><img alt="" src="/images/news/PG7%20(1).jpg" /></div>
+</div>
+
+</div>
