6f7123675225c115d366d3cd2357ab5de72be8b5
prowebdev728 <proweb.developer2016@yandex.com>
2017-08-28 23:23:28 +0800

    add news page

---
+++ views/news/kube_video_production.php
@@ -0,0 +1,21 @@
+<div class="news">
+	<h5>Lights, Camera, KUBE Action!</h5>
+	<div class="image text-center">
+		<img src="/images/kube-video.jpg" alt="KUBE Video Starts Production">
+	</div>
+	<p>There was a bit of excitement buzzing around the Color Compass Coquitlam office last week as we approached filming day for our 6 month in the making, highly anticipated, promotional KUBE video. With the help of Ballistic Arts, one of Vancouver’s most creative digital marketing agency, we transformed the newly completed Color Compass Training Centre into a mini body shop and filming set.</p>
+	<p>Leading the shoot was Ballistic Arts' video director, Jarvis Nigelsky with Jon West behind the lens and Roswell Cho on standby. Color Compass' Chris Neale was front and center as the main talent for the day, acting as a collision repair technician, as Chris put it "acting" is a very key term. But, Chris wasn’t the only familiar face debuting his acting skills las week. Throughout our 8 hour shoot, we had a few guest appearances on set. Special thanks to Natasha Regina (KUBE trainer), Brent Dutka (Shop Manager), Daniel Rodrigue (Refinish Tech) and Kyle Kushnir (Refinish Tech) for taking time out of their day, suiting up and playing supporting roles. They don’t call us Hollywood North for nothing.</p>
+	<p>Our goal for this video is to use it to introduce our customers and staff to KUBE, demonstrate how intuitive it is to use, and most importantly, highlight the benefits of using the system to improve their businesses, all within 90 seconds. Once complete, it will be promoted across all of our various social media and marketing platforms, and will aid as a selling tool when meeting with customers one on one or in a tradeshow environment.</p>
+	<p>The Marketing department has been working on this project since late 2016, and we look forward to sharing the final product with you! Stay tuned.</p>
+	<div class="row small-up-2 medium-up-4 padded-top">
+		<div class="column padded-vertical"><img src="/images/kube-video2.jpg" alt="KUBE Video Starts Production"></div>
+		<div class="column padded-vertical"><img src="/images/kube-video3.jpg" alt="KUBE Video Starts Production"></div>
+		<div class="column padded-vertical"><img src="/images/kube-video4.jpg" alt="KUBE Video Starts Production"></div>
+		<div class="column padded-vertical"><img src="/images/kube-video6.jpg" alt="KUBE Video Starts Production"></div>
+	</div>
+	<div class="row small-up-2 medium-up-4 padded-top">
+		<div class="column padded-vertical"><img src="/images/kube-video5.jpg" alt="KUBE Video Starts Production"></div>
+		<div class="column padded-vertical"><img src="/images/kube-video1.jpg" alt="KUBE Video Starts Production"></div>
+	</div>
+
+</div>
